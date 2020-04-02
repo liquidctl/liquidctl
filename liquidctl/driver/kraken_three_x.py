132eb50d0ce3f7eceb6227251446c88744eda434
Tom Frey <tom.frey@informatik.hu-berlin.de>
2020-04-03 00:27:05 +0200

    implement lighting control (everything except super-* and wings mode)

--- liquidctl/driver/kraken_three_x.py
+++ liquidctl/driver/kraken_three_x.py
@@ -40,6 +40,7 @@
  """
 
 import logging
+import itertools
 
 from liquidctl.driver.usb import UsbHidDriver
 
@@ -52,6 +53,72 @@ _MAX_READ_ATTEMPTS = 12
 _COLOR_CHANNELS = {
     'ring': 0x02,
     'logo': 0x04,
+}
+_COLOR_MODES = {
+    # (mode, size/variant, speed scale, min colors, max colors)
+    'off':                                  (0x00, 0x00, 0, 0, 0),
+    'fixed':                                (0x00, 0x00, 0, 1, 1),
+    'fading':                               (0x01, 0x00, 1, 1, 8),
+    'spectrum-wave':                        (0x02, 0x00, 2, 0, 0),
+    'backwards-spectrum-wave':              (0x02, 0x00, 2, 0, 0),
+    'marquee-3':                            (0x03, 0x03, 2, 1, 1),
+    'marquee-4':                            (0x03, 0x04, 2, 1, 1),
+    'marquee-5':                            (0x03, 0x05, 2, 1, 1),
+    'marquee-6':                            (0x03, 0x06, 2, 1, 1),
+    'backwards-marquee-3':                  (0x03, 0x03, 2, 1, 1),
+    'backwards-marquee-4':                  (0x03, 0x04, 2, 1, 1),
+    'backwards-marquee-5':                  (0x03, 0x05, 2, 1, 1),
+    'backwards-marquee-6':                  (0x03, 0x06, 2, 1, 1),
+    'covering-marquee':                     (0x04, 0x00, 2, 1, 8),
+    'covering-backwards-marquee':           (0x04, 0x00, 2, 1, 8),
+    'alternating-3':                        (0x05, 0x03, 3, 1, 2),
+    'alternating-4':                        (0x05, 0x04, 3, 1, 2),
+    'alternating-5':                        (0x05, 0x05, 3, 1, 2),
+    'alternating-6':                        (0x05, 0x06, 3, 1, 2),
+    'moving-alternating-3':                 (0x05, 0x03, 4, 1, 2),
+    'moving-alternating-4':                 (0x05, 0x04, 4, 1, 2),
+    'moving-alternating-5':                 (0x05, 0x05, 4, 1, 2),
+    'moving-alternating-6':                 (0x05, 0x06, 4, 1, 2),
+    'backwards-moving-alternating-3':       (0x05, 0x03, 4, 1, 2),
+    'backwards-moving-alternating-4':       (0x05, 0x04, 4, 1, 2),
+    'backwards-moving-alternating-5':       (0x05, 0x05, 4, 1, 2),
+    'backwards-moving-alternating-6':       (0x05, 0x06, 4, 1, 2),
+    'pulse':                                (0x06, 0x00, 5, 1, 8),
+    'breathing':                            (0x07, 0x00, 6, 1, 8),
+    'candle':                               (0x08, 0x00, 0, 1, 1),
+    'starry-night':                         (0x09, 0x00, 5, 1, 1),
+    'rainbow-flow':                         (0x0b, 0x00, 2, 0, 0),
+    'super-rainbow':                        (0x0c, 0x00, 2, 0, 0),
+    'rainbow-pulse':                        (0x0d, 0x00, 2, 0, 0),
+    'backwards-rainbow-flow':               (0x0b, 0x00, 2, 0, 0),
+    'backwards-super-rainbow':              (0x0c, 0x00, 2, 0, 0),
+    'backwards-rainbow-pulse':              (0x0b, 0x00, 2, 0, 0),
+    'loading':                              (0x10, 0x00, 8, 1, 1),
+    'tai-chi':                              (0x0e, 0x00, 7, 1, 2),
+    # 'water-cooler':                         (0x0f, 0x00, 6, 0, 0),
+}
+_STATIC_VALUE = {
+    0x02: 0x08,
+    0x04: 0x01
+}
+_SPEED_VALUE = {
+    # (slowest, slow, normal, fast, fastest)
+    0: ([0x32, 0x00], [0x32, 0x00], [0x32, 0x00], [0x32, 0x00], [0x32, 0x00]),
+    1: ([0x50, 0x00], [0x3c, 0x00], [0x28, 0x00], [0x14, 0x00], [0x0a, 0x00]),
+    2: ([0x5e, 0x01], [0x2c, 0x01], [0xfa, 0x00], [0x96, 0x00], [0x50, 0x00]),
+    3: ([0x40, 0x06], [0x14, 0x05], [0xe8, 0x03], [0x20, 0x03], [0x58, 0x02]),
+    4: ([0x20, 0x03], [0xbc, 0x02], [0xf4, 0x01], [0x90, 0x01], [0x2c, 0x01]),
+    5: ([0x19, 0x00], [0x14, 0x00], [0x0f, 0x00], [0x07, 0x00], [0x04, 0x00]),
+    6: ([0x28, 0x00], [0x1e, 0x00], [0x14, 0x00], [0x0a, 0x00], [0x04, 0x00]),
+    7: ([0x32, 0x00], [0x28, 0x00], [0x1e, 0x00], [0x14, 0x00], [0x0a, 0x00]),
+    8: ([0x14, 0x00], [0x14, 0x00], [0x14, 0x00], [0x14, 0x00], [0x14, 0x00]),
+}
+_ANIMATION_SPEEDS = {
+    'slowest': 0x0,
+    'slower': 0x1,
+    'normal': 0x2,
+    'faster': 0x3,
+    'fastest': 0x4,
 }
 _ACCESSORY_NAMES = {
     0x01: "HUE+ LED Strip",
@@ -66,6 +133,7 @@ _ACCESSORY_NAMES = {
     0x10: "Kraken X3 Pump Ring",
     0x11: "Kraken X3 Pump Logo",
 }
+
 
 class KrakenThreeX(UsbHidDriver):
     """liquidctl driver for Kraken X3 devices from NZXT."""
@@ -128,6 +196,24 @@ class KrakenThreeX(UsbHidDriver):
             ('Pump duty', msg[19], '%'),
         ]
 
+    def set_color(self, channel, mode, colors, speed='normal', **kwargs):
+        """Set the color mode for a specific channel."""
+        cid = _COLOR_CHANNELS[channel]
+        _, _, _, mincolors, maxcolors = _COLOR_MODES[mode]
+        colors = [[g, r, b] for [r, g, b] in colors]
+        if len(colors) < mincolors:
+            raise ValueError('Not enough colors for mode={}, at least {} required'.format(mode, mincolors))
+        elif maxcolors == 0:
+            if colors:
+                LOGGER.warning('too many colors for mode=%s, none needed', mode)
+            colors = [[0, 0, 0]]  # discard the input but ensure at least one step
+        elif len(colors) > maxcolors:
+            LOGGER.warning('too many colors for mode=%s, dropping to %i', mode, maxcolors)
+            colors = colors[:maxcolors]
+        sval = _ANIMATION_SPEEDS[speed]
+        self._write_colors(cid, mode, colors, sval)
+        self.device.release()
+
     def set_fixed_speed(self, channel, duty, **kwargs):
         """Set channel to a fixed speed duty."""
         if channel != 'pump':
@@ -164,6 +250,40 @@ class KrakenThreeX(UsbHidDriver):
         assert False, f'missing messages (attempts={_MAX_READ_ATTEMPTS}, missing={len(parsers)})'
 
     def _write(self, data):
-        padding = [0x0]*(_WRITE_LENGTH - len(data))
+        padding = [0x0] * (_WRITE_LENGTH - len(data))
         LOGGER.debug('write %s (and %i padding bytes)', ' '.join(format(i, '02x') for i in data), len(padding))
         self.device.write(data + padding)
+
+    def _write_colors(self, cid, mode, colors, sval):
+        mval, size_variant, speed_scale, mincolors, maxcolors = _COLOR_MODES[mode]
+        color_count = len(colors)
+        if maxcolors == 40:
+            raise NotImplementedError()
+        elif mode == 'wings':  # wings requires special handling
+            raise NotImplementedError()
+        else:
+            opcode = [0x2a, 0x04]
+            address = [cid, cid]
+            speed_value = _SPEED_VALUE[speed_scale][sval]
+            header = opcode + address + [mval] + speed_value
+            color = list(itertools.chain(*colors)) + [0, 0, 0] * (16 - color_count)
+            if 'marquee' in mode:
+                backwards_byte = 0x04
+            elif mode == 'starry-night' or 'moving-alternating' in mode:
+                backwards_byte = 0x01
+            else:
+                backwards_byte = 0x00
+            if 'backwards' in mode:
+                backwards_byte += 0x02
+            if mode == 'fading' or mode == 'pulse' or mode == 'breathing':
+                mode_related = 0x08
+            elif mode == 'tai-chi' or mode == 'water-cooler':
+                mode_related = 0x05
+            elif mode == 'loading':
+                mode_related = 0x04
+            else:
+                mode_related = 0x00
+            static_byte = _STATIC_VALUE[cid]
+            led_size = size_variant if mval == 0x03 or mval == 0x05 else 0x03
+            footer = [backwards_byte, color_count, mode_related, static_byte, led_size]
+            self._write(header + color + footer)
