0e80f4db759c121d7d908a78ae0a8e87ce5d418c
Jonas Malaco <jonas@protocubo.io>
2020-06-13 01:32:41 -0300

    Try to enable simplified fan speed control

--- liquidctl/driver/coolit_platinum.py
+++ liquidctl/driver/coolit_platinum.py
@@ -1,4 +1,4 @@
-"""liquidctl driver for CoolIT Platinum coolers for Corsair.
+"""liquidctl driver for Corsair Platinum and PRO XT coolers.
 
 Supported devices
 -----------------
@@ -6,13 +6,16 @@ Supported devices
  - [ ] Corsair H100i Platinum SE
  - [✓] Corsair H100i Platinum
  - [✓] Corsair H115i Platinum
+ - [✓] Corsair H100i PRO XT
+ - [✓] Corsair H115i PRO XT
+ - [ ] Corsair H150i PRO XT
 
 Supported features
 ------------------
 
- - [ ] general monitoring
- - [ ] pump speed control
- - [ ] fan speed control
+ - [✓] general monitoring
+ - [·] pump speed control
+ - [·] fan speed control
  - [ ] lighing control
 
 Copyright (C) 2020–2020  Jonas Malaco
@@ -22,57 +25,122 @@ SPDX-License-Identifier: GPL-3.0-or-later
 """
 
 import logging
+import itertools
 
 from liquidctl.driver.usb import UsbHidDriver
+from liquidctl.keyval import RuntimeStorage
 from liquidctl.pmbus import compute_pec
+from liquidctl.util import clamp
 
 
 LOGGER = logging.getLogger(__name__)
 
 _READ_LENGTH = 64
 _WRITE_LENGTH = 64
-_WRITE_PREFIX = b'\x3f'
+_HEADER_LENGTH = 3
+_TRAILER_LENGTH = 1
+_DATA_LENGTH = _WRITE_LENGTH - _HEADER_LENGTH - _TRAILER_LENGTH
+
+_WRITE_PREFIX = 0x3F
+_FEATURE_COOLING = 0x0
+_CMD_GET_STATUS = 0xFF
+_CMD_SET_COOLING = 0x14
+_CMD_SET_LIGHTING1 = 0b100
+_CMD_SET_LIGHTING2 = 0b100
+
+_FAN1_DATA_OFFSET = 0xB - _HEADER_LENGTH
+_FAN2_DATA_OFFSET = 0x11 - _HEADER_LENGTH
+_PUMP_DATA_OFFSET = 0x14 - _HEADER_LENGTH
+
+# TODO replace with enums and add modes
+_FAN_MODE_FIXED_DUTY = 0x2
+_PUMP_MODE_BALANCED = 0x1
 
 
 class CoolitPlatinumDriver(UsbHidDriver):
-    """liquidctl driver for CoolIT Platinum coolers for Corsair."""
+    """liquidctl driver for Corsair Platinum and PRO XT coolers."""
 
     SUPPORTED_DEVICES = [
-        # (0x1b1c, ??, None, 'Corsair H100i Platinum SE (experimental)', {}),
-        (0x1b1c, 0x0c18, None, 'Corsair H100i Platinum (experimental)', {}),
-        (0x1b1c, 0x0c17, None, 'Corsair H115i Platinum (experimental)', {}),
+        # (0x1B1C, ??, None, 'Corsair H100i Platinum SE (experimental)', {}),
+        (0x1B1C, 0x0C18, None, 'Corsair H100i Platinum (experimental)', {}),
+        (0x1B1C, 0x0C17, None, 'Corsair H115i Platinum (experimental)', {}),
+        (0x1B1C, 0x0C20, None, 'Corsair H100i PRO XT (experimental)', {}),
+        (0x1B1C, 0x0C21, None, 'Corsair H115i PRO XT (experimental)', {}),
+        # Note: unknown adjustments needed to support three fans (H150i PRO XT)
     ]
+
+    def __init__(self, device, description, **kwargs):
+        super().__init__(device, description, **kwargs)
+        self._sequence = itertools.count()
+        self._data = None  # only initialize in connect
+
+    def connect(self, **kwargs):
+        super().connect(**kwargs)
+        ids = '{:04x}_{:04x}'.format(self.vendor_id, self.product_id)
+        # FIXME uniquely identify specific units of the same model
+        self._data = RuntimeStorage(key_prefixes=[ids])
 
     def initialize(self, **kwargs):
         """Initialize the device.
 
-        Aparently not required.
+        While the device itself is not know to require any initialization
+        commands, this driver needs to store some data, and that should be
+        initialized.
         """
-        pass
+        self._data.store_int('fan1_duty', None)
+        self._data.store_int('fan2_duty', None)
 
     def get_status(self, **kwargs):
         """Get a status report.
 
         Returns a list of `(property, value, unit)` tuples.
         """
-        msg = self._call([0x78, 0xff])
+        msg = self._call(_CMD_GET_STATUS, feature=_FEATURE_COOLING)
         return [
-            ('Liquid temperature', msg[7] / 256 + msg[8], '°C'),
-            ('Fan 1 speed', msg[16] << 8 | msg[15], 'rpm'),
-            ('Fan 2 speed', msg[23] << 8 | msg[22], 'rpm'),
-            ('Pump speed', msg[30] << 8 | msg[29], 'rpm'),
-            ('Firmware version', f'{msg[2] >> 4}.{msg[2] & 0xf}.{msg[3]}', '')
+            ('Liquid temperature', msg[8] + msg[7] / 255, '°C'),
+            ('Fan 1 speed', int.from_bytes(msg[15:18], byteorder='little'), 'rpm'),
+            ('Fan 2 speed', int.from_bytes(msg[22:24], byteorder='little'), 'rpm'),
+            ('Pump speed', int.from_bytes(msg[29:31], byteorder='little'), 'rpm'),
+            ('Firmware version', f'{msg[2] >> 4}.{msg[2] & 0xf}.{msg[3]}', ''),
         ]
 
-    def _call(self, data):
-        buf = bytearray(_WRITE_PREFIX)
-        buf.extend(data)
-        buf.zfill(_WRITE_LENGTH)
+    def set_fixed_speed(self, channel, duty, **kwargs):
+        channel = channel.lower()
+        if channel not in ['fan1', 'fan2']:
+            raise ValueError("Only channels supported channels are fan1 and fan2")
+        self._data.store_int(f'{channel}_duty', clamp(duty, 0, 100))
+        self._send_set_cooling()
+
+    def _call(self, command, feature=None, data=None):
+        buf = bytearray(_WRITE_LENGTH)
+        buf[0] = _WRITE_PREFIX
+        buf[1] = (next(self._sequence) % 31 + 1) << 3
+        if feature is not None:
+            buf[1] |= feature
+            buf[2] = command
+        else:
+            buf[1] |= command
+        if data:
+            buf[_HEADER_LENGTH:-1] = data
         buf[-1] = compute_pec(buf[1:-1])
-        LOGGER.debug('write %s', buf.hex())  # FIXME lazily call buf.hex()
+        LOGGER.debug('write %s', buf.hex())
+        self.device.clear_enqueued_reports()
         self.device.write(buf)
 
         buf = bytes(self.device.read(_READ_LENGTH))
         self.device.release()
-        LOGGER.debug('received %s', buf.hex())  # FIXME lazily call buf.hex()
+        LOGGER.debug('received %s', buf.hex())
+        # TODO check response PEC
         return buf
+
+    def _send_set_cooling(self):
+        data = bytearray(_DATA_LENGTH)
+        data[_FAN1_DATA_OFFSET] = _FAN_MODE_FIXED_DUTY
+        fan1_duty = clamp(self._data.load_int('fan1_duty', default=100), 0, 100)
+        data[_FAN1_DATA_OFFSET + 5] = int(fan1_duty * 2.55)
+        data[_FAN2_DATA_OFFSET] = _FAN_MODE_FIXED_DUTY
+        fan2_duty = clamp(self._data.load_int('fan2_duty', default=100), 0, 100)
+        data[_FAN2_DATA_OFFSET + 5] = int(fan2_duty * 2.55)
+        data[_PUMP_DATA_OFFSET] = _PUMP_MODE_BALANCED
+        self._call(_CMD_SET_COOLING, feature=_FEATURE_COOLING, data=data)
+        # TODO try to assert something on the response
