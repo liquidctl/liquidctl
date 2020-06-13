e87d52788b5312221f5b6a700e4639533821cf64
Jonas Malaco <jonas@protocubo.io>
2020-06-13 01:32:07 -0300

    Use SPDX short identifier instead of standard GPL notice

--- liquidctl/driver/coolit_platinum.py
+++ liquidctl/driver/coolit_platinum.py
@@ -15,26 +15,10 @@ Supported features
  - [ ] fan speed control
  - [ ] lighing control
 
----
-
-liquidctl driver for CoolIT Platinum coolers for Corsair.
 Copyright (C) 2020–2020  Jonas Malaco
 Copyright (C) 2020–2020  each contribution's author
 
-This file is part of liquidctl.
-
-liquidctl is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-liquidctl is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
+SPDX-License-Identifier: GPL-3.0-or-later
 """
 
 import logging
