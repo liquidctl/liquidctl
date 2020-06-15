liquidctl/liquidctl

commit 33a456d26834c1932da7e4257d201290bf84b30a

Ensure the store profile is a list
Reported-by: @makk50

liquidctl/driver/coolit_platinum.py
@@ -214,7 +214,7 @@ class CoolitPlatinumDriver(UsbHidDriver):
             raise ValueError(f"Unknown channel, should be of: 'fan', {''.join(self._fans)}")
         for channel in channels:
             self._data.store(f'{channel}_mode', FanMode.CUSTOM_PROFILE.value)
-            self._data.store(f'{channel}_profile', profile)
+            self._data.store(f'{channel}_profile', list(profile))
         self._send_set_cooling()
 
     def set_color(self, channel, mode, colors, **kwargs):
@@ -248,7 +248,7 @@ class CoolitPlatinumDriver(UsbHidDriver):
 
         Animations always require successive calls to this API.
         """
-        def warn_if_extra_colors(limit):
+        def warn_if_extra_colors(liait):
             if len(colors) > limit:
                 LOGGER.warning('too many colors for channel=%s and mode=%s, dropping to %d',
                                channel, mode, limit)

commit d67e43c3d31623274985356bf5aba2addd1ada36

Fix typo introduced in 33a456d

liquidctl/driver/coolit_platinum.py
@@ -248,7 +248,7 @@ class CoolitPlatinumDriver(UsbHidDriver):
 
         Animations always require successive calls to this API.
         """
-        def warn_if_extra_colors(liait):
+        def warn_if_extra_colors(limit):
             if len(colors) > limit:
                 LOGGER.warning('too many colors for channel=%s and mode=%s, dropping to %d',
                                channel, mode, limit)