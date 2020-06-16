21e707899dd59d7ebcf906594e8b6f79b418f8f1
Jonas Malaco <jonas@protocubo.io>
2020-06-15 22:16:00 -0300

    Store all mock-sent report in a deque

--- tests/test_corsair_platinum.py
+++ tests/test_corsair_platinum.py
@@ -1,6 +1,9 @@
 import unittest
+from collections import deque, namedtuple
 
 from liquidctl.driver.coolit_platinum import CoolitPlatinumDriver
+
+_Report = namedtuple('_Report', ['number', 'data'])
 
 
 def _noop(*args, **kwargs):
@@ -14,8 +17,7 @@ class _MockPlatinumDevice:
         self.fan1_speed = 1499
         self.fan2_speed = 1512
         self.pump_speed = 2702
-        self.sent_report_number = None
-        self.sent_data = None
+        self.sent = deque()
 
         self.vendor_id = vid
         self.product_id = pid
@@ -37,8 +39,7 @@ class _MockPlatinumDevice:
         return buf[:length]
 
     def write(self, data):
-        self.sent_report_number = data[0]
-        self.sent_data = bytes(data[1:])
+        self.sent.appendleft(_Report(data[0], bytes(data[1:])))
         return len(data) - 1
 
 
@@ -60,8 +61,8 @@ class CorsairPlatinumTestCase(unittest.TestCase):
         self.assertEqual(fan1[1], self.mock_hid.fan1_speed)
         self.assertEqual(fan2[1], self.mock_hid.fan2_speed)
         self.assertEqual(pump[1], self.mock_hid.pump_speed)
-        self.assertEqual(self.mock_hid.sent_data[1] & 0b111, 0)
-        self.assertEqual(self.mock_hid.sent_data[2], 0xff)
+        self.assertEqual(self.mock_hid.sent[0].data[1] & 0b111, 0)
+        self.assertEqual(self.mock_hid.sent[0].data[2], 0xff)
 
     def test_initialize_status(self):
         (fw_version, ) = self.device.initialize()
@@ -69,28 +70,28 @@ class CorsairPlatinumTestCase(unittest.TestCase):
 
     def test_set_pump_mode(self):
         self.device.initialize(pump_mode='extreme')
-        self.assertEqual(self.mock_hid.sent_data[1] & 0b111, 0)
-        self.assertEqual(self.mock_hid.sent_data[2], 0x14)
-        self.assertEqual(self.mock_hid.sent_data[0x17], 0x2)
+        self.assertEqual(self.mock_hid.sent[0].data[1] & 0b111, 0)
+        self.assertEqual(self.mock_hid.sent[0].data[2], 0x14)
+        self.assertEqual(self.mock_hid.sent[0].data[0x17], 0x2)
 
     def test_fixed_fan_speeds(self):
         self.device.set_fixed_speed(channel='fan', duty=42)
         self.device.set_fixed_speed(channel='fan2', duty=84)
-        self.assertEqual(self.mock_hid.sent_data[1] & 0b111, 0)
-        self.assertEqual(self.mock_hid.sent_data[2], 0x14)
-        self.assertEqual(self.mock_hid.sent_data[0x0b], 0x2)
-        self.assertAlmostEqual(self.mock_hid.sent_data[0x10] / 2.55, 42, delta=1 / 2.55)
-        self.assertEqual(self.mock_hid.sent_data[0x11], 0x2)
-        self.assertAlmostEqual(self.mock_hid.sent_data[0x16] / 2.55, 84, delta=1 / 2.55)
+        self.assertEqual(self.mock_hid.sent[0].data[1] & 0b111, 0)
+        self.assertEqual(self.mock_hid.sent[0].data[2], 0x14)
+        self.assertEqual(self.mock_hid.sent[0].data[0x0b], 0x2)
+        self.assertAlmostEqual(self.mock_hid.sent[0].data[0x10] / 2.55, 42, delta=1 / 2.55)
+        self.assertEqual(self.mock_hid.sent[0].data[0x11], 0x2)
+        self.assertAlmostEqual(self.mock_hid.sent[0].data[0x16] / 2.55, 84, delta=1 / 2.55)
 
     def test_custom_fan_profiles(self):
         self.device.set_speed_profile(channel='fan', profile=[(20, 0), (55, 100)])
         self.device.set_speed_profile(channel='fan2', profile=[(30, 20), (50, 80)])
-        self.assertEqual(self.mock_hid.sent_data[1] & 0b111, 0)
-        self.assertEqual(self.mock_hid.sent_data[2], 0x14)
-        self.assertEqual(self.mock_hid.sent_data[0x0b], 0x0)
-        self.assertEqual(list(self.mock_hid.sent_data[0x1e:0x2c]),
+        self.assertEqual(self.mock_hid.sent[0].data[1] & 0b111, 0)
+        self.assertEqual(self.mock_hid.sent[0].data[2], 0x14)
+        self.assertEqual(self.mock_hid.sent[0].data[0x0b], 0x0)
+        self.assertEqual(list(self.mock_hid.sent[0].data[0x1e:0x2c]),
                          [20, 0, 55, 255] + 5 * [60, 255])
-        self.assertEqual(self.mock_hid.sent_data[0x11], 0x0)
-        self.assertEqual(list(self.mock_hid.sent_data[0x2c:0x3a]),
+        self.assertEqual(self.mock_hid.sent[0].data[0x11], 0x0)
+        self.assertEqual(list(self.mock_hid.sent[0].data[0x2c:0x3a]),
                          [30, 51, 50, 204, 60, 255] + 4 * [60, 255])
