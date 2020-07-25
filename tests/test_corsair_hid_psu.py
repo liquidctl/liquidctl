501f0eadcc45c152b2981e36230376c3799a8668
Jonas Malaco <jonas@protocubo.io>
2020-07-25 13:37:22 -0300

    corsair_hid_psu: tests: Ensure the drivers are never completely broken

---
+++ tests/test_corsair_hid_psu.py
@@ -0,0 +1,28 @@
+import unittest
+from liquidctl.driver.corsair_hid_psu import CorsairHidPsu
+from liquidctl.driver.corsair_hid_psu import _CORSAIR_12V_OCP_MODE
+from liquidctl.driver.corsair_hid_psu import _CORSAIR_FAN_CONTROL_MODE
+from _testutils import MockHidapiDevice, Report
+
+
+class _MockPsuDevice(MockHidapiDevice):
+    def write(self, data):
+        reply = bytearray(64)
+        if data[1] in [_CORSAIR_12V_OCP_MODE, _CORSAIR_FAN_CONTROL_MODE]:
+            reply[2] = 1  # just a valid mode
+        self.preload_read(Report(0, reply))  # ignore report IDs for now
+
+
+class CorsairHidPsuTestCase(unittest.TestCase):
+    def setUp(self):
+        self.mock_usb = _MockPsuDevice()
+        self.device = CorsairHidPsu(self.mock_usb, 'Mock Corsair HID PSU')
+        self.device.connect()
+
+    def tearDown(self):
+        self.device.disconnect()
+
+    def test_not_totally_broken(self):
+        """A few reasonable example calls do not raise exceptions."""
+        self.device.initialize()
+        status = self.device.get_status()
