4569f58facae2750889aceca7e4aaed7a5ed5bde
Jonas Malaco <jonas@protocubo.io>
2020-08-01 04:04:40 -0300

    usb: rewrite usb.core.Device construction test with mocked APIs
    On systems where a compatible device is present but not accessible with
    the current user's permissions, trying to fetch the device's serial
    number cause an unrelated error.
    
    Rewrite the test to no longer depend on a physical device.
    
    Reported-by: Sergey Kasmy

--- tests/test_backwards_compatibility_11.py
+++ tests/test_backwards_compatibility_11.py
@@ -1,21 +1,33 @@
-"""Test compatiblitity with liquidctl 1.1.0."""
+"""Test backwards compatibility with liquidctl 1.1.0."""
 
-import unittest
 from liquidctl.driver.kraken2 import Kraken2
+from liquidctl.driver.usb import hid, HidapiDevice
+import usb
 
-SYSTEM_HAS_KRAKEN2 = len(Kraken2.find_supported_devices()) > 0
 
-class Pre12RawPyUsbHandles(unittest.TestCase):
-    @unittest.skipUnless(SYSTEM_HAS_KRAKEN2, "requires a physical/real Kraken X2")
-    def test_construct_with_raw_pyusb_handle(self):
-        import usb
+class _MockPyUsbHandle(usb.core.Device):
+    def __init__(self, serial_number):
+        self.idVendor = 0x1e71
+        self.idProduct = 0x170e
+        self._serial_number = serial_number
 
-        USB_VID_NZXT = 0x1E71
-        USB_PID_KRAKENX_GEN3 = 0x170E
 
-        pyusb_device = usb.core.find(idVendor=USB_VID_NZXT, idProduct=USB_PID_KRAKENX_GEN3)
-        liquidctl_device = Kraken2(pyusb_device, 'Some device')
-        self.assertEqual(liquidctl_device.device.serial_number, pyusb_device.serial_number,
-                          msg='<driver instance>.device not usable')
-        self.assertNotIsInstance(liquidctl_device.device, usb.core.Device,
-                                 msg='<driver instance>.device not wrapped/converted')
+def _mock_enumerate(vendor_id=0, product_id=0):
+    return [
+        {'vendor_id': vendor_id, 'product_id': product_id, 'serial_number': '987654321'},
+        {'vendor_id': vendor_id, 'product_id': product_id, 'serial_number': '123456789'}
+    ]
+
+
+def test_construct_with_raw_pyusb_handle(monkeypatch):
+    monkeypatch.setattr(hid, 'enumerate', _mock_enumerate)
+    pyusb_handle = _MockPyUsbHandle(serial_number='123456789')
+    liquidctl_device = Kraken2(pyusb_handle, 'Some device')
+    assert liquidctl_device.device.vendor_id == pyusb_handle.idVendor, \
+            '<driver instance>.device points to incorrect physical device'
+    assert liquidctl_device.device.product_id == pyusb_handle.idProduct, \
+            '<driver instance>.device points to incorrect physical device'
+    assert liquidctl_device.device.serial_number == pyusb_handle.serial_number, \
+            '<driver instance>.device points to different physical unit'
+    assert isinstance(liquidctl_device.device, HidapiDevice), \
+            '<driver instance>.device not properly converted to HidapiDevice instance'
