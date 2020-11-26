90d7ec50d7235c58b6ee0d16e602763ed2a1bb9a
Jonas Malaco <jonas@protocubo.io>
2020-11-26 09:57:57 -0300

    pytest: enable NVIDIA tests on all platforms as they use emulation

--- conftest.py
+++ conftest.py
@@ -4,7 +4,6 @@ collect_ignore = ['setup.py']
 
 if sys.platform != 'linux':
     collect_ignore.append('tests/test_smbus.py')
-    collect_ignore.append('tests/test_nvidia.py')
 
 if sys.platform not in ['win32', 'cyqwin']:
     collect_ignore.append('extra/windows/LQiNFO.py')
