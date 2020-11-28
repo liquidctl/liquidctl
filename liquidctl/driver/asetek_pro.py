74490833c44e5db252ed9478d486a528feb0be9e
Andrew Robertson <andytank123@gmail.com>
2020-11-28 13:36:58 +0000

    Remove some duplicate code from Asetek Pro driver and improve testing

--- liquidctl/driver/asetek_pro.py
+++ liquidctl/driver/asetek_pro.py
@@ -20,10 +20,10 @@ from liquidctl.driver.asetek import Modern690Lc
 from liquidctl.driver.usb import UsbDriver
 from liquidctl.error import NotSupportedByDevice
 from liquidctl.keyval import RuntimeStorage
-from liquidctl.util import clamp
+from liquidctl.util import clamp, LazyHexRepr
 
 
-LOGGER = logging.getLogger(__name__)
+_LOGGER = logging.getLogger(__name__)
 
 _MAX_PROFILE_POINTS = 7
 _CRITICAL_TEMPERATURE = 60
@@ -41,6 +41,7 @@ _PRO_CMD_WRITE_FAN_SPEED = 0x40
 _PRO_CMD_READ_PUMP_SPEED = 0x31
 _PRO_CMD_READ_PUMP_MODE = 0x33
 _PRO_CMD_WRITE_PUMP_MODE = 0x32
+_PRO_CMD_WRITE_COLOR_LIST = 0x56
 _PRO_PUMP_MODES = [
     'Quiet',
     'Balanced',
@@ -70,6 +71,15 @@ _PRO_SPEEDS = {
     ]
 }
 
+_PRO_COLOR_CHANGE_MODES = {
+    'rainbow': [0x55, 0x01],
+    'alert': [],
+    'shitf': [0x55, 0x01],
+    'pulse': [0x52, 0x01],
+    'blinking': [0x58, 0x01],
+    'fixed': [0x55, 0x01]
+}
+
 
 class CorsairAsetekProDriver(Modern690Lc):
     """liquidctl driver for Corsair-branded sixth generation Asetek coolers."""
@@ -83,7 +93,7 @@ class CorsairAsetekProDriver(Modern690Lc):
 
     def _write(self, data):
         """Write data to the AIO and log"""
-        LOGGER.debug('write %s', ' '.join(format(i, '02x') for i in data))
+        _LOGGER.debug('write %s', LazyHexRepr(data))
         self.device.write(_PRO_WRITE_ENDPOINT, data, _WRITE_TIMEOUT)
 
     # Not calling this (or begin transaction) since they do not seem to be needed for H100i Pro
@@ -96,7 +106,7 @@ class CorsairAsetekProDriver(Modern690Lc):
         approach.
         """
         msg = self.device.read(_PRO_READ_ENDPOINT, length, _READ_TIMEOUT)
-        LOGGER.debug('received %s', ' '.join(format(i, '02x') for i in msg))
+        _LOGGER.debug('received %s', LazyHexRepr(msg))
         self.device.release()
         return msg
 
@@ -114,38 +124,36 @@ class CorsairAsetekProDriver(Modern690Lc):
         colorCount = 0
         setColors = []
         for color in colors:
-            LOGGER.debug('color %s', color)
             colorCount = colorCount + 1
             setColors = setColors + color
-        self._write([0x56, colorCount] + setColors)
+        self._write([_PRO_CMD_WRITE_COLOR_LIST, colorCount] + setColors)
         self._end_transaction_and_read(3)
 
     def _get_fan_speeds(self):
-        """Read the RPM speed of the fans. This Tries to get the speeds for 3 fans as that is
-        how many the H150i PRO has"""
+        """Read the RPM speed of the fans. Try to get the speeds for 3 fans as that is
+        how many the H150i PRO supports"""
         speeds = []
         for i in [0, 1, 2]:
             self._write([_PRO_CMD_READ_FAN_SPEED, i])
             msg = self._end_transaction_and_read(6)
             if msg[0] != 0x41 or msg[1] != 0x12 or msg[2] != 0x34 or msg[3] != i:
-                LOGGER.debug('Unable to get speed for fan id {}'.format(i))
+                _LOGGER.debug('Unable to get speed for fan id %d', i)
                 continue
-            speeds.append((msg[4] << 8) + msg[5])
+            speeds.append((i, (msg[4] << 8) + msg[5]))
         return speeds
 
     def _get_fan_indexes(self, channel):
         if len(channel) > 3:
             return [int(channel[3:]) - 1]
-        return range(len(self._get_fan_speeds()))
+        return [reading[0] for reading in self._get_fan_speeds()]
 
     def initialize(self, pump_mode=None, **kwargs):
         """Initialize the device."""
-        LOGGER.debug('Pro configure device...')
+        _LOGGER.debug('Pro configure device...')
         if pump_mode == None:
             return
         else:
-            LOGGER.debug('pump_mode %s write value %s', pump_mode,
-                format(_PRO_PUMP_MODES.index(pump_mode),'02x'))
+            _LOGGER.debug('pump_mode %s', pump_mode)
             self._write([_PRO_CMD_WRITE_PUMP_MODE, _PRO_PUMP_MODES.index(pump_mode)])
             self._end_transaction_and_read(5)
 
@@ -157,27 +165,22 @@ class CorsairAsetekProDriver(Modern690Lc):
         ### Can some Pro devices have more than one temp sensor?   OCL seems to code for that?
         self._write([_PRO_CMD_READ_AIO_TEMP])
         msg = self._end_transaction_and_read(6)
-        #LOGGER.debug('aio temp msg %s', ' '.join(format(i, '02x') for i in msg))
         aio_temp = msg[3] + msg[4]/10
         status.append(('Liquid temperature', aio_temp, '°C'))
         speeds = self._get_fan_speeds()
-        for i in range(len(speeds)):
-            status.append(('Fan {} speed'.format(i+1), speeds[i] | 0, 'rpm'))
+        for reading in speeds:
+            status.append(('Fan {} speed'.format(reading[0]), reading[1] | 0, 'rpm'))
         self._write([_PRO_CMD_READ_PUMP_MODE])
         msg = self._end_transaction_and_read(4)
-        #LOGGER.debug('pump mode msg %s', ' '.join(format(i, '02x') for i in msg))
         pump_mode = msg[3]
         status.append(('Pump mode', _PRO_PUMP_MODES[pump_mode], ""))
         self._write([_PRO_CMD_READ_PUMP_SPEED])
         msg = self._end_transaction_and_read(5)
-        #LOGGER.debug('pump speed msg %s', ' '.join(format(i, '02x') for i in msg))
         pump_speed = (msg[3] << 8) + msg[4]
         status.append(('Pump speed', pump_speed | 0, 'rpm'))
         self._write([_PRO_CMD_READ_FIRMWARE])
         msg = self._end_transaction_and_read(7)
-        #LOGGER.debug('firmware id msg %s', ' '.join(format(i, '02x') for i in msg))
-        firmware = '{}.{}.{}.{}'.format(format(msg[3], '01x'), format(msg[4], '01x'),
-            format(msg[5], '01x'),format(msg[6], '01x'))
+        firmware = '{}.{}.{}.{}'.format(*tuple(msg[3:7]))
         status.append(('Firmware version', firmware, ''))
         return status
 
@@ -185,44 +188,25 @@ class CorsairAsetekProDriver(Modern690Lc):
                   alert_threshold=_HIGH_TEMPERATURE, alert_color=[255, 0, 0],
                   speed=3, temps=['35', '45', '55'], **kwargs):
         """Set the color mode for a specific channel."""
-        LOGGER.debug('kwargs %s', kwargs)
+        if mode not in _PRO_COLOR_CHANGE_MODES:
+            raise KeyError('Unknown lighting mode {}'.format(mode))
         colors = list(colors)
-        LOGGER.debug('color count = %d', len(colors))
-        LOGGER.debug('speed %s', int(speed))
-        if mode == 'rainbow':
-            self._write_color_change_speed(mode, speed)
-            self._write([0x55, 0x01])
-            self._end_transaction_and_read(3)
-        elif mode == 'shift':
-            self._write_color(colors)
-            self._write_color_change_speed(mode, speed)
-            self._write([0x55, 0x01])
-            self._end_transaction_and_read(3)
-        elif mode == 'pulse':
-            self._write_color(colors)
-            self._write_color_change_speed(mode, speed)
-            self._write([0x52, 0x01])
-            self._end_transaction_and_read(3)
-        elif mode == 'blinking':
-            self._write_color(colors)
-            self._write_color_change_speed(mode, speed)
-            self._write([0x58, 0x01])
-            self._end_transaction_and_read(3)
-        elif mode == 'fixed':
-            self._write_color([colors[0], colors[0]])
-            self._write([0x55, 0x01])
-            self._end_transaction_and_read(3)
-        elif mode == 'alert':
-            temps = list(temps.split(' '))
-            LOGGER.debug('temps count = %d', len(temps))
-            LOGGER.debug('asetek temps %s %s %s', temps[0], temps[1], temps[2])
-            LOGGER.debug('color count = %d', len(colors))
+        _LOGGER.debug('color count = %d', len(colors))
+        _LOGGER.debug('color change speed %d', int(speed))
+        if mode == 'alert':
             self._write([0x5f, int(temps[0]), 0x00, int(temps[1]), 0x00, int(temps[2]), 0x00] + colors[0] + colors[1] + colors[2])
             self._end_transaction_and_read(6)
             self._write([0x5E, 0x01])
             self._end_transaction_and_read(3)
-        else:
-            raise KeyError('Unknown lighting mode {}'.format(mode))
+            return
+        if mode == 'fixed':
+            colors = [colors[0], colors[0]]
+        if mode != 'rainbow':
+            self._write_color(colors)
+        if mode != 'fixed':
+            self._write_color_change_speed(mode, speed)
+        self._write(_PRO_COLOR_CHANGE_MODES[mode])
+        self._end_transaction_and_read(3)
 
     def set_speed_profile(self, channel, profile, **kwargs):
         """Set channel to follow a speed duty profile."""
@@ -230,13 +214,14 @@ class CorsairAsetekProDriver(Modern690Lc):
             raise ValueError('set_speed_profile not implemented for the pump on CorsairAsetekPro')
         adjusted = self._prepare_profile(profile, 0, 100, _MAX_PROFILE_POINTS)
         for temp, duty in adjusted:
-            LOGGER.info('setting %s PWM point: (%i°C, %i%%), device interpolated',
+            _LOGGER.info('setting %s PWM point: (%i°C, %i%%), device interpolated',
                         channel, temp, duty)
         temps, duties = map(list, zip(*adjusted))
         # Need to write curve for each fan
         # Assume if we read n values from _get_fan_speed the fans are numbered [0, n)
         # This will have problems on a 2 fan system when the first fan has failed
         for i in self._get_fan_indexes(channel):
+            _LOGGER.info('setting speed for fan %d', i+1)
             self._write([_PRO_CMD_WRITE_FAN_SPEED, i] + temps + duties)
             self._end_transaction_and_read(32)
 
@@ -246,7 +231,7 @@ class CorsairAsetekProDriver(Modern690Lc):
             # Setting a fix speed is supported, however it is done by sending RPM or PWM
             # values. I'm not 100% sure how this maps to %s used by liquidclt so set a flat
             # speed profile instead.
-            LOGGER.info('using a flat profile to set %s to a fixed duty', channel)
+            _LOGGER.info('using a flat profile to set %s to a fixed duty', channel)
             self.set_speed_profile(channel, [(0, duty), (_CRITICAL_TEMPERATURE - 1, duty)])
             return
         # Fixed speed pump is not supported, so guess a curve from the duty value given
