879144f6477c584eda64e005653fc4b533eb4c1a
Andrew Robertson <andytank123@gmail.com>
2020-12-01 16:46:48 +0000

    Make changes to Corsair Pro driver from pull request comments

--- liquidctl/driver/asetek_pro.py
+++ liquidctl/driver/asetek_pro.py
@@ -12,11 +12,12 @@ and Chris Griffith.
 SPDX-License-Identifier: GPL-3.0-or-later
 """
 
+import itertools
 import logging
 
 import usb
 
-from liquidctl.driver.asetek import Modern690Lc
+from liquidctl.driver.asetek import _CommonAsetekDriver
 from liquidctl.driver.usb import UsbDriver
 from liquidctl.error import NotSupportedByDevice
 from liquidctl.keyval import RuntimeStorage
@@ -37,15 +38,16 @@ _PRO_READ_ENDPOINT = 0x81
 _PRO_CMD_READ_FIRMWARE = 0xaa
 _PRO_CMD_READ_AIO_TEMP = 0xa9
 _PRO_CMD_READ_FAN_SPEED = 0x41
-_PRO_CMD_WRITE_FAN_SPEED = 0x40
+_PRO_CMD_WRITE_FAN_CURVE = 0x40
+_PRO_CMD_WRITE_FAN_SPEED = 0x42
 _PRO_CMD_READ_PUMP_SPEED = 0x31
 _PRO_CMD_READ_PUMP_MODE = 0x33
 _PRO_CMD_WRITE_PUMP_MODE = 0x32
 _PRO_CMD_WRITE_COLOR_LIST = 0x56
 _PRO_PUMP_MODES = [
-    'Quiet',
-    'Balanced',
-    'Performance'
+    'quiet',
+    'balanced',
+    'performance'
     ]
 
 _PRO_SPEEDS = {
@@ -81,7 +83,7 @@ _PRO_COLOR_CHANGE_MODES = {
 }
 
 
-class CorsairAsetekProDriver(Modern690Lc):
+class CorsairAsetekProDriver(_CommonAsetekDriver):
     """liquidctl driver for Corsair-branded sixth generation Asetek coolers."""
     SUPPORTED_DEVICES = [
         (0x1b1c, 0x0c12, None, 'Corsair Hydro H150i Pro', {}),
@@ -112,20 +114,15 @@ class CorsairAsetekProDriver(Modern690Lc):
 
     def _write_color_change_speed(self, mode, speed):
         """Send the speed to cycle colors on the RGB pump"""
-        if speed < 1 or speed > 3:
-            raise ValueError('Unsupported speed ({}), only 1-3 supported'.format(speed))
-
+        speed = clamp(speed, 1, 3)
         self._write([0x53, _PRO_SPEEDS[mode][int(speed)-1]])
         self._end_transaction_and_read(3)
 
     def _write_color(self, colors):
         """Write a list of colors to cycle between, the cycle pattern comes from the
         value used for cycle speed"""
-        colorCount = 0
-        setColors = []
-        for color in colors:
-            colorCount = colorCount + 1
-            setColors = setColors + color
+        setColors = list(itertools.chain(*colors))
+        colorCount = int(len(setColors) / 3)
         self._write([_PRO_CMD_WRITE_COLOR_LIST, colorCount] + setColors)
         self._end_transaction_and_read(3)
 
@@ -133,7 +130,7 @@ class CorsairAsetekProDriver(Modern690Lc):
         """Read the RPM speed of the fans. Try to get the speeds for 3 fans as that is
         how many the H150i PRO supports"""
         speeds = []
-        for i in [0, 1, 2]:
+        for i in range(3):
             self._write([_PRO_CMD_READ_FAN_SPEED, i])
             msg = self._end_transaction_and_read(6)
             if msg[0] != 0x41 or msg[1] != 0x12 or msg[2] != 0x34 or msg[3] != i:
@@ -147,29 +144,26 @@ class CorsairAsetekProDriver(Modern690Lc):
             return [int(channel[3:]) - 1]
         return [reading[0] for reading in self._get_fan_speeds()]
 
-    def initialize(self, pump_mode=None, **kwargs):
+    def initialize(self, pump_mode='balanced', **kwargs):
         """Initialize the device."""
         _LOGGER.debug('Pro configure device...')
-        if pump_mode == None:
-            return
-        else:
-            _LOGGER.debug('pump_mode %s', pump_mode)
-            self._write([_PRO_CMD_WRITE_PUMP_MODE, _PRO_PUMP_MODES.index(pump_mode)])
-            self._end_transaction_and_read(5)
+        _LOGGER.debug('pump_mode %s', pump_mode)
+        self._write([_PRO_CMD_WRITE_PUMP_MODE, _PRO_PUMP_MODES.index(pump_mode.lower())])
+        self._end_transaction_and_read(5)
 
     def get_status(self, **kwargs):
         """Get a status report.
         Returns a list of `(property, value, unit)` tuples.
         """
         status = []
-        ### Can some Pro devices have more than one temp sensor?   OCL seems to code for that?
         self._write([_PRO_CMD_READ_AIO_TEMP])
         msg = self._end_transaction_and_read(6)
         aio_temp = msg[3] + msg[4]/10
         status.append(('Liquid temperature', aio_temp, '°C'))
         speeds = self._get_fan_speeds()
         for reading in speeds:
-            status.append(('Fan {} speed'.format(reading[0]), reading[1] | 0, 'rpm'))
+            fan_num = reading[0] + 1
+            status.append((f'Fan {fan_num} speed', reading[1], 'rpm'))
         self._write([_PRO_CMD_READ_PUMP_MODE])
         msg = self._end_transaction_and_read(4)
         pump_mode = msg[3]
@@ -177,7 +171,7 @@ class CorsairAsetekProDriver(Modern690Lc):
         self._write([_PRO_CMD_READ_PUMP_SPEED])
         msg = self._end_transaction_and_read(5)
         pump_speed = (msg[3] << 8) + msg[4]
-        status.append(('Pump speed', pump_speed | 0, 'rpm'))
+        status.append(('Pump speed', pump_speed, 'rpm'))
         self._write([_PRO_CMD_READ_FIRMWARE])
         msg = self._end_transaction_and_read(7)
         firmware = '{}.{}.{}.{}'.format(*tuple(msg[3:7]))
@@ -189,7 +183,7 @@ class CorsairAsetekProDriver(Modern690Lc):
                   speed=3, temps=['35', '45', '55'], **kwargs):
         """Set the color mode for a specific channel."""
         if mode not in _PRO_COLOR_CHANGE_MODES:
-            raise KeyError('Unknown lighting mode {}'.format(mode))
+            raise KeyError(f'Unknown lighting mode {mode}')
         colors = list(colors)
         _LOGGER.debug('color count = %d', len(colors))
         _LOGGER.debug('color change speed %d', int(speed))
@@ -210,47 +204,37 @@ class CorsairAsetekProDriver(Modern690Lc):
 
     def set_speed_profile(self, channel, profile, **kwargs):
         """Set channel to follow a speed duty profile."""
+        channel = channel.lower()
         if channel == 'pump':
-            raise ValueError('set_speed_profile not implemented for the pump on CorsairAsetekPro')
+            raise NotSupportedByDevice()
+        if not channel.startswith('fan'):
+            raise KeyError(f'Unknown channel: {channel}')
         adjusted = self._prepare_profile(profile, 0, 100, _MAX_PROFILE_POINTS)
         for temp, duty in adjusted:
             _LOGGER.info('setting %s PWM point: (%i°C, %i%%), device interpolated',
                         channel, temp, duty)
         temps, duties = map(list, zip(*adjusted))
-        # Need to write curve for each fan
-        # Assume if we read n values from _get_fan_speed the fans are numbered [0, n)
-        # This will have problems on a 2 fan system when the first fan has failed
+        # Need to write curve for each fan in channel
         for i in self._get_fan_indexes(channel):
             _LOGGER.info('setting speed for fan %d', i+1)
-            self._write([_PRO_CMD_WRITE_FAN_SPEED, i] + temps + duties)
+            self._write([_PRO_CMD_WRITE_FAN_CURVE, i] + temps + duties)
             self._end_transaction_and_read(32)
 
     def set_fixed_speed(self, channel, duty, **kwargs):
         """Set channel to a fixed speed duty."""
+        channel = channel.lower()
         if channel.startswith('fan'):
-            # Setting a fix speed is supported, however it is done by sending RPM or PWM
-            # values. I'm not 100% sure how this maps to %s used by liquidclt so set a flat
-            # speed profile instead.
-            _LOGGER.info('using a flat profile to set %s to a fixed duty', channel)
-            self.set_speed_profile(channel, [(0, duty), (_CRITICAL_TEMPERATURE - 1, duty)])
-            return
-        # Fixed speed pump is not supported, so guess a curve from the duty value given
-        # less than 50 == quiet (0), less than 80 == balanced (1), greater than 80 == proformance (2).
-        # Arbitrary values are arbitrary.
-        if duty < 50:
-            duty = 0
-        elif duty < 80:
-            duty = 1
+            duty = clamp(duty, 0, 100)
+            # Need to write curve for each fan in channel
+            for i in self._get_fan_indexes(channel):
+                _LOGGER.info('setting speed for fan %d to %d', i+1, duty)
+                self._write([_PRO_CMD_WRITE_FAN_SPEED, i, duty])
+                self._end_transaction_and_read(32)
+        elif channel == 'pump':
+            raise NotSupportedByDevice()
         else:
-            duty = 2
-        self._write([_PRO_CMD_WRITE_PUMP_MODE, duty])
-        self._end_transaction_and_read(5)
-        # OCL Reads the mode after setting it, so do the same
-        self._write([_PRO_CMD_READ_PUMP_MODE])
-        self._end_transaction_and_read(4)
+            raise KeyError(f'Unknow channel: {channel}')
 
     @classmethod
-    def probe(cls, handle, legacy_690lc=False, **kwargs):
-        # the modern driver overrides probe and rigs it to switch on
-        # --legacy-690lc, so we override it again
-        return super().probe(handle, legacy_690lc=False, **kwargs)
+    def probe(cls, handle, **kwargs):
+        return super().probe(handle, **kwargs)
