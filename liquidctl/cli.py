liquidctl/liquidctl

commit 4ee1cb37b06a78ca5ae5dcd2ec9ee8ad22a153a7

cli: improve error reporting and process all devices before aborting

liquidctl/cli.py
@@ -70,6 +70,7 @@ import inspect
 import logging
 import os
 import sys
+import errno
 
 from docopt import docopt
 
@@ -297,10 +298,18 @@ def main():
     elif len(selected) == 0:
         raise SystemExit('Error: no devices matches available drivers and selection criteria')
 
+    errors = 0
+
+    def log_error(err, msg, *args):
+        nonlocal errors
+        errors += 1
+        LOGGER.info('%s', err, exc_info=True)
+        LOGGER.error(msg, *args)
+
     for dev in selected:
         LOGGER.debug('device: %s', dev.description)
-        dev.connect(**opts)
         try:
+            dev.connect(**opts)
             if args['initialize']:
                 _print_dev_status(dev, dev.initialize(**opts))
             elif args['status']:
@@ -311,18 +320,27 @@ def main():
                 _device_set_color(dev, args, **opts)
             else:
                 raise Exception('Not sure what to do')
-        except NotSupportedByDevice:
-            raise SystemExit(f'Error: operation not supported by {dev.description}')
-        except NotSupportedByDriver:
-            raise SystemExit(f'Error: operation not supported by driver for {dev.description}')
+        except OSError as err:
+            # each backend API returns a different subtype of OSError (OSError, usb.core.USBError
+            # or PermissionError), but all set err.errno to EACCES (or EPERM)
+            if err.errno in [errno.EACCES , errno.EPERM]:
+                log_error(err, f'Error: insufficient permissions to access {dev.description}')
+            else:
+                log_error(err, 'Unexpected OS error with %s', dev.description)
+        except NotSupportedByDevice as err:
+            log_error(err, f'Error: operation not supported by {dev.description}')
+        except NotSupportedByDriver as err:
+            log_error(err, f'Error: operation not supported by driver for {dev.description}')
         except UnsafeFeaturesNotEnabled as err:
             features = ','.join(err.args)
-            raise SystemExit(f'Error: missing --unsafe features for {dev.description}: {features!r}')
-        except:
-            LOGGER.exception('Unexpected error with %s', dev.description)
-            sys.exit(1)
+            log_error(err, f'Error: missing --unsafe features for {dev.description}: {features!r}')
+        except Exception as err:
+            log_error(err, 'Unexpected error with %s', dev.description)
         finally:
             dev.disconnect(**opts)
+
+    if errors:
+        sys.exit(errors)
 
 
 def find_all_supported_devices(**opts):

commit 77f2b5c76364b73f714b1c38c2b333a15d23a41a

cli: append formated exception to unknown error messages
Exception.__str__ merely formats the exception arguments, but not its
type, which can be next to useless.

$ liquidctl -m smart set fan speed 90
ERROR: Unexpected error with NZXT Smart Device (V1): 'fan'

To fix, use traceback.format_exception instead.

$ liquidctl -m smart set fan speed 90
ERROR: Unexpected error with NZXT Smart Device (V1): KeyError: 'fan'

Fixes: 4ee1cb37b06a ("cli: improve error reporting and process all
devices before aborting")
Reported-by: u/nimbleping (on reddit)

liquidctl/cli.py
@@ -69,6 +69,7 @@ import inspect
 import logging
 import os
 import sys
+from traceback import format_exception
 
 from docopt import docopt
 
@@ -302,11 +303,15 @@ def main():
 
     errors = 0
 
-    def log_error(err, msg, *args):
+    def log_error(err, msg, append_err=False, *args):
         nonlocal errors
         errors += 1
         _LOGGER.info('%s', err, exc_info=True)
-        _LOGGER.error(msg, *args)
+        if append_err:
+            exception = list(format_exception(Exception, err, None))[-1].rstrip()
+            _LOGGER.error(f'{msg}: {exception}', *args)
+        else:
+            _LOGGER.error(msg, *args)
 
     for dev in selected:
         _LOGGER.debug('device: %s', dev.description)
@@ -330,7 +335,7 @@ def main():
             elif err.args == ('open failed', ):
                 log_error(err, f'Error: could not open {dev.description}, possibly due to insufficient permissions')
             else:
-                log_error(err, f'Unexpected OS error with {dev.description}: {err}')
+                log_error(err, f'Unexpected OS error with {dev.description}', append_err=True)
         except NotSupportedByDevice as err:
             log_error(err, f'Error: operation not supported by {dev.description}')
         except NotSupportedByDriver as err:
@@ -340,7 +345,7 @@ def main():
             log_error(err, f'Error: missing --unsafe features for {dev.description}: {features!r}')
             _LOGGER.error('More information is provided in the corresponding device guide')
         except Exception as err:
-            log_error(err, f'Unexpected error with {dev.description}: {err}')
+            log_error(err, f'Unexpected error with {dev.description}', append_err=True)
         finally:
             dev.disconnect(**opts)
 