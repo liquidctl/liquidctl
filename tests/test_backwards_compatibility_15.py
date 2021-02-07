5836abbc180727c5cd3d7ba315e419be15e88fa8
Jonas Malaco <jonas@protocubo.io>
2021-02-07 11:44:39 -0300

    tests: backwards_compaibility_15: fill entire mock hidinfo dictionary

--- tests/test_backwards_compatibility_15.py
+++ tests/test_backwards_compatibility_15.py
@@ -8,21 +8,32 @@ from liquidctl.driver.usb import hid
 
 def test_matches_platinum_and_pro_xt_coolers_regardless_of_hydro(monkeypatch):
     mock_match = [
-        {'vendor_id': 0x1b1c, 'product_id': 0x0c18, 'usage_page': 0},  # H100i Platinum
-        {'vendor_id': 0x1b1c, 'product_id': 0x0c19, 'usage_page': 0},  # H100i Platinum SE
-        {'vendor_id': 0x1b1c, 'product_id': 0x0c17, 'usage_page': 0},  # H115i Platinum
-        {'vendor_id': 0x1b1c, 'product_id': 0x0c20, 'usage_page': 0},  # H100i Pro XT
-        {'vendor_id': 0x1b1c, 'product_id': 0x0c21, 'usage_page': 0},  # H115i Pro XT
-        {'vendor_id': 0x1b1c, 'product_id': 0x0c22, 'usage_page': 0},  # H150i Pro XT
+        {'vendor_id': 0x1b1c, 'product_id': 0x0c18},  # H100i Platinum
+        {'vendor_id': 0x1b1c, 'product_id': 0x0c19},  # H100i Platinum SE
+        {'vendor_id': 0x1b1c, 'product_id': 0x0c17},  # H115i Platinum
+        {'vendor_id': 0x1b1c, 'product_id': 0x0c20},  # H100i Pro XT
+        {'vendor_id': 0x1b1c, 'product_id': 0x0c21},  # H115i Pro XT
+        {'vendor_id': 0x1b1c, 'product_id': 0x0c22},  # H150i Pro XT
     ]
 
     mock_skip = [
-        {'vendor_id': 0x1b1c, 'product_id': 0xffff, 'usage_page': 0},  # nothing
-        {'vendor_id': 0xffff, 'product_id': 0x0c22, 'usage_page': 0},  # nothing
+        {'vendor_id': 0x1b1c, 'product_id': 0xffff},  # nothing
+        {'vendor_id': 0xffff, 'product_id': 0x0c22},  # nothing
     ]
 
+    ignored_infos = {
+        'path': b'',
+        'serial_number': '',
+        'release_number': 0,
+        'manufacturer_string': '',
+        'product_string': '',
+        'usage_page': 0,
+        'usage': 0,
+        'interface_number': 0
+    }
+
     def mock_enumerate(vid=0, pid=0):
-        return mock_match + mock_skip
+        return [infos | ignored_infos for infos in mock_match + mock_skip]
 
     monkeypatch.setattr(hid, 'enumerate', mock_enumerate)
 
