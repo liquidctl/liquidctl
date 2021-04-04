3335ca34c163476f062e1b8c3b9f75a672032338
Marshall Asch <maasch@rogers.com>
2021-04-04 18:40:44 -0400

    added inital led validation block

--- liquidctl/config/validation.py
+++ liquidctl/config/validation.py
@@ -1,4 +1,29 @@
+import logging
+from liquidctl.util import color_from_str
 
+_LOGGER = logging.getLogger(__name__)
 
 def _validate_leds(block):
-    pass
+    isValid = True
+
+    if not block.get('mode'):
+        _LOGGER.warning('led mode is a required field')
+        isValid = False
+
+    colors = block.get('colors')
+    if colors:
+        if not isinstance(colors, list):
+            _LOGGER.warning('led colors must be a list of colors')
+            isValid = False
+        else:
+            for c in colors:
+                if not isinstance(c, str):
+                    _LOGGER.warning('led color must be a list of color strings')
+                    isValid = False
+                else:
+                    try:
+                        color_from_str(c)
+                    except ValueError:
+                        isValid = False
+
+    return isValid
--- tests/test_config_validation.py
+++ tests/test_config_validation.py
@@ -27,6 +27,12 @@ def test_led_block_required_fields(parse_test_file):
     valid = _validate_leds(ledBlock)
     assert valid
 
+def test_led_block_missing_mode(parse_test_file):
+    ledBlock = parse_test_file('led_missing_mode.toml')
+
+    valid = _validate_leds(ledBlock)
+    assert not valid
+
 def test_led_block_missing_colors_fields(parse_test_file):
     ledBlock = parse_test_file('led_missing_colors_fields.toml')
 
@@ -36,7 +42,6 @@ def test_led_block_missing_colors_fields(parse_test_file):
 @pytest.mark.parametrize("config", [
     'led_colors_hex_list.toml',
     'led_colors_invalid_hex.toml',
-    'led_colors_empty.toml',
     'led_colors_string.toml'
 ], indirect=['config'])
 def test_led_block_invalid_colors(config):
@@ -49,6 +54,7 @@ def test_led_block_invalid_colors(config):
     'led_colors_hex_str.toml',
     'led_colors_hls_str.toml',
     'led_colors_hsv_str.toml',
+    'led_colors_empty.toml',
     'led_colors_num_hex_str.toml',
     'led_colors_rgb_str.toml'
 ], indirect=['config'])
