d3551d818e095e63670c37c05632c785d0a41f93
Sander Bilo <sander.bilo@gmail.com>
2021-07-30 15:50:00 +0200

    smart_device: enable new variant of Smart Device V2 (#364)

--- liquidctl/driver/smart_device.py
+++ liquidctl/driver/smart_device.py
@@ -331,6 +331,10 @@ class SmartDevice2(_CommonSmartDeviceDriver):
             'speed_channel_count': 3,
             'color_channel_count': 2
         }),
+        (0x1e71, 0x200f, None, 'NZXT Smart Device V2', {
+            'speed_channel_count': 3,
+            'color_channel_count': 2
+        }),
         (0x1e71, 0x2001, None, 'NZXT HUE 2', {
             'speed_channel_count': 0,
             'color_channel_count': 4
