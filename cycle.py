856af97c65c489d807afb7067614cc0aae62353b
Peter Eckersley <pde@objective.is>
2022-02-08 23:25:57 -0800

    Rewrite against the API

--- cycle.py
+++ cycle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import liquidctl.cli as lc
+import liquidctl
 import coloraide as c
 import argparse
 
@@ -30,27 +31,36 @@ if len(colors) < 2:
 if len(colors) < 2:
     colors.append(bluer)
 
-sys.argv[1:] = ["set", "led6", "color", "color-cycle", "--speed", "slower"]
+devs = liquidctl.find_liquidctl_devices()
+for dev in devs:
+    if "Gigabyte RGB Fusion 2.0" in dev.description:
+        break
+else:
+    print("No Gigabyte RGB Fusion 2.0 device found.")
+    sys.exit(1)
 
 STEPS = 30
-sys.argv[1:] = ["--debug"] if args.debug else []
-sys.argv += "set led6 color fixed ff0000".split()
-lc.main()
+
 lookup = []
+
+def to_int(color):
+    string = color.to_string() # "rgb(255 75 0)"
+    string = string.partition("(")[2][:-1] # "255 75 0"
+    string = string.split() # ["255", "75", "0"]
+    return [int(float(x)) for x in string]
 
 for i in range(len(colors) - 1):
     gradient = colors[i].interpolate(colors[i + 1], space=args.space)
-    lookup += [gradient(x / STEPS).to_string(hex=True) for x in range(STEPS)]
+    cols= [to_int(gradient(x / STEPS)) for x in range(STEPS)]
+    lookup += cols
 
 
 lookup += reversed(lookup)
-print(lookup)
+if args.debug:
+    print("Cycling through", lookup)
 while True:
     for x in range(2 * STEPS):
         c = lookup[x]
-        # keeping these hook variables in the liquidctl.cli module lets us
-        # call back into _device_set_color() with minimal overhead
-        lc.hook_args["<color>"] = [c[1:]]
-        with lc.hook_dev.connect(**lc.hook_opts):
-            lc._device_set_color(lc.hook_dev, lc.hook_args, **lc.hook_opts)
-        time.sleep(0.25 / STEPS)
+        with dev.connect():
+            dev.set_color(channel=args.channel, mode='fixed', colors=[c])
+        time.sleep(2.0 / STEPS)
