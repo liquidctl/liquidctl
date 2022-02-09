3b48a52ef9d7c69eeacb7118aebe05ca6f963b3e
Peter Eckersley <pde@objective.is>
2022-02-08 22:24:11 -0800

    Very hacky implementation of color fading
    for a device that doesn't support it

---
+++ cycle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import liquidctl.cli as lc
+import coloraide as c
+import argparse
+
+import time
+import sys
+
+teal = c.Color("#2de544")
+# bluer = c.Color("#2dca7d")
+bluer = c.Color("#2d8a8d")
+deep_blue = c.Color("#001030")
+purple = c.Color("#c000c0")
+red = c.Color("#d00000")
+
+# set up argparse
+parser = argparse.ArgumentParser(description="Cycle through a list of colors")
+parser.add_argument("--space", type=str, default="srgb", help="Color space to use")
+# you can provide 0 or more colors to cycle through
+parser.add_argument("colors", nargs="*", help="Colors to cycle through")
+parser.add_argument("--debug", action="store_true", help="Print debug messages")
+args = parser.parse_args()
+
+colors = [c.Color("#" + x) for x in args.colors]
+if len(colors) < 2:
+    colors.append(teal)
+if len(colors) < 2:
+    colors.append(bluer)
+
+
+sys.argv[1:] = ["set", "led6", "color", "color-cycle", "--speed", "slower"]
+
+STEPS = 30
+sys.argv[1:] = ["--debug"] if args.debug else []
+sys.argv += "set led6 color fixed ff0000".split()
+lc.main()
+lookup = []
+
+for i in range(len(colors) - 1):
+    gradient = colors[i].interpolate(colors[i + 1], space=args.space)
+    lookup += [gradient(x / STEPS).to_string(hex=True) for x in range(STEPS)]
+
+
+lookup += reversed(lookup)
+print(lookup)
+while True:
+    for x in range(2 * STEPS):
+        c = lookup[x]
+        lc.hook_args["<color>"] = [c[1:]]
+        with lc.hook_dev.connect(**lc.hook_opts):
+            lc._device_set_color(lc.hook_dev, lc.hook_args, **lc.hook_opts)
+        time.sleep(0.25 / STEPS)
