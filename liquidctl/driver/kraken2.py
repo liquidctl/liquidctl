liquidctl/liquidctl

commit ab9b010b461bf70a8b9f77aefc738887e3c793fb

kraken2: remove old support for connecting with initialize()
This behavior was deprecated in 1.1.0rc1 (3+ years ago).  GKraken was
the only known user, but it has already been updated.

As a consequence of removing initialize(), finalize() is also removed.

This change is not visible to users of the CLI.

liquidctl/driver/kraken2.py
@@ -125,11 +125,6 @@ class Kraken2(UsbHidDriver):
         or `disconnect()`.
         """
 
-        # before v1.1 `initialize` was used to connect to the device; that has
-        # since been deprecated, but we have to support that usage until v2
-        if not self._connected:
-            self.connect(**kwargs)
-
         # read early but only once, since self.supports_cooling_profiles can
         # indirectly reuse this one; no need to clear old reports since the
         # firmware version can be assumed to be constant for the lifetime of
@@ -145,12 +140,6 @@ class Kraken2(UsbHidDriver):
 
         firmware = '{}.{}.{}'.format(*self._firmware_version)
         return [('Firmware version', firmware, '')]
-
-    def finalize(self):
-        """Deprecated."""
-        _LOGGER.warning('deprecated: use disconnect() instead')
-        if self._connected:
-            self.disconnect()
 
     def get_status(self, **kwargs):
         """Get a status report.

tests/test_backward_compatibility_10.py
@@ -6,11 +6,8 @@ community.
 """
 
 import pytest
-from _testutils import MockHidapiDevice
-from test_kraken2 import _MockKrakenDevice
-from test_kraken2 import mockKrakenXDevice as mockConnectedDevice
+from test_kraken2 import mockKrakenXDevice
 
-from liquidctl.driver.kraken_two import KrakenTwoDriver
 from liquidctl.version import __version__
 
 SPECTRUM = [
@@ -30,31 +27,6 @@ def test_pre11_apis_find_does_not_raise():
     liquidctl.cli.find_all_supported_devices()
 
 
-def test_pre11_apis_deprecated_super_mode(mockConnectedDevice):
+def test_pre11_apis_deprecated_super_mode(mockKrakenXDevice):
     # deprecated in favor of super-fixed, super-breathing and super-wave
-    mockConnectedDevice.set_color('sync', 'super', [(128, 0, 255)] + SPECTRUM, 'normal')
-
-
-def test_pre11_apis_status_order(mockConnectedDevice):
-    # GKraken unreasonably expects a particular ordering
-    pass
-
-
-@pytest.fixture(params=[False, True], ids=['not connected', 'connected'])
-def mockDevice(request):
-    device = _MockKrakenDevice(fw_version=(6, 0, 2))
-    dev = KrakenTwoDriver(device, 'Mock X62', device_type=KrakenTwoDriver.DEVICE_KRAKENX)
-
-    if request.param:
-        dev.connect()
-    return dev
-
-
-def test_pre11_apis_connect_with_initialize(mockDevice):
-    # deprecated behavior in favor of connect()
-    mockDevice.initialize()
-
-
-def test_pre11_apis_disconnect_with_finalize(mockDevice):
-    # deprecated in favor of disconnect()
-    mockDevice.finalize()
+    mockKrakenXDevice.set_color('sync', 'super', [(128, 0, 255)] + SPECTRUM, 'normal')

commit 49bcc7be70f429a321a3b47ba22876ffa5498d90

kraken2: remove unused code left over from "connect via initialize"
Fixes: ab9b010b461b ("kraken2: remove old support for connecting with initialize()")

liquidctl/driver/kraken2.py
@@ -106,16 +106,6 @@ class Kraken2(UsbHidDriver):
         self.supports_lighting = True
         self.supports_cooling = self.device_type != self.DEVICE_KRAKENM
         self._firmware_version = None  # read once necessary
-        self._connected = False
-
-    def connect(self, **kwargs):
-        ret = super().connect(**kwargs)
-        self._connected = True
-        return ret
-
-    def disconnect(self, **kwargs):
-        super().disconnect(**kwargs)
-        self._connected = False
 
     def initialize(self, **kwargs):
         """Initialize the device and the driver.