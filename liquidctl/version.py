liquidctl/liquidctl

commit d2258de5a6142e4d6344574ad43ece7110242b37

version: move on if setuptools_scm is not available
Fixes: 053921a6e2e3 ("version: prevent membership test attempt on None")

liquidctl/version.py
@@ -3,20 +3,24 @@
 # uses the psf/black style
 
 
-def _get_version_relaxed():
+def _get_built_version():
 
-    # try to get the version written by setuptools_scm during the build
     try:
         from liquidctl._version import version, version_tuple
 
         return (version, version_tuple)
     except ModuleNotFoundError:
-        pass
+        return None
 
-    # if that failed, try to compute the version directly
-    from setuptools_scm import get_version
 
-    # first assuming that we're a git checkout
+def _compute_version_at_runtime():
+
+    try:
+        from setuptools_scm import get_version
+    except ModuleNotFoundError:
+        return None
+
+    # first, assume that we're a git checkout
     try:
         version = get_version()
         if version:
@@ -24,7 +28,7 @@ def _get_version_relaxed():
     except LookupError:
         pass
 
-    # and then, if that also failed, assuming that we're a tarball
+    # if that also failed, assume that we're a tarball
     try:
         guess = get_version(parentdir_prefix_version="liquidctl-")
         if guess:
@@ -36,14 +40,14 @@ def _get_version_relaxed():
     except LookupError:
         pass
 
-    # finally, use a obviously invalid value
+    # finally, use an obviously invalid value
     return ("0.0.0-unknown", None)
 
 
-# keep _version_tuple private for now, as it's only available in some cases and
-# don't want to commit to it yet
-
-(version, _version_tuple) = _get_version_relaxed()
+# try to get the version written by setuptools_scm during the build, otherwise
+# compute one right now; _version_tuple is kept private as it's only available
+# in some cases and don't want to commit to it yet
+(version, _version_tuple) = _get_built_version() or _compute_version_at_runtime()
 
 # old field name (liquidctl.__version__ is preferred now)
 __version__ = version

commit 2da4f9ca4a79fdbe4e6bc50c644e67dbfb31237d

version: separate fallible operations from final fallback value
Fixes: d2258de5a614 ("version: move on if setuptools_scm is not available")

liquidctl/version.py
@@ -3,7 +3,7 @@
 # uses the psf/black style
 
 
-def _get_built_version():
+def _build_version():
 
     try:
         from liquidctl._version import version, version_tuple
@@ -13,7 +13,7 @@ def _get_built_version():
         return None
 
 
-def _compute_version_at_runtime():
+def _runtime_version():
 
     try:
         from setuptools_scm import get_version
@@ -40,14 +40,16 @@ def _compute_version_at_runtime():
     except LookupError:
         pass
 
-    # finally, use an obviously invalid value
-    return ("0.0.0-unknown", None)
+    return None
 
 
-# try to get the version written by setuptools_scm during the build, otherwise
-# compute one right now; _version_tuple is kept private as it's only available
-# in some cases and don't want to commit to it yet
-(version, _version_tuple) = _get_built_version() or _compute_version_at_runtime()
+# - try to get the version written by setuptools_scm during the build;
+# - otherwise try to compute one right now;
+# - failing that too, use an obviously invalid value
+#
+# (_version_tuple is kept private as it's only available in some cases and
+# don't want to commit to it yet)
+(version, _version_tuple) = _build_version() or _runtime_version() or ("0.0.0-unknown", None)
 
 # old field name (liquidctl.__version__ is preferred now)
 __version__ = version