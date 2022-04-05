liquidctl/liquidctl

commit 5dc22aa260922fe4dc940401e32ab9f605fe351b

kraken2: get status from hwmon if possible
The firmware version is *temporary* brought back to get_status() as a
migration aid for GKraken,[^1] but is hidden from CLI users in
anticipation of its permanent removal.

Related: #403
[^1]: https://gitlab.com/leinardi/gkraken/-/issues/45

liquidctl/driver/kraken2.py
@@ -36,6 +36,11 @@ _SPEED_CHANNELS = {  # (base, minimum duty, maximum duty)
     'pump':  (0xc0, 0, 100),
 }
 
+_STATUS_TEMPERATURE = 'Liquid temperature'
+_STATUS_FAN_SPEED = 'Fan speed'
+_STATUS_PUMP_SPEED = 'Pump speed'
+_STATUS_FWVERSION = 'Firmware version'
+
 # more aggressive than observed 4.0.3 and 6.0.2 firmware defaults
 _RESET_FAN_PROFILE = [(20, 25), (30, 50), (50, 90), (60, 100)]
 _RESET_PUMP_PROFILE = [(20, 50), (30, 60), (40, 90), (50, 100)]
@@ -129,7 +134,45 @@ class Kraken2(UsbHidDriver):
         firmware = '{}.{}.{}'.format(*self.firmware_version)
         return [('Firmware version', firmware, '')]
 
-    def get_status(self, **kwargs):
+    def _get_status_directly(self, with_firmware):
+        # the firmware version is duplicated here as a temporary migration aid
+        # for GKraken; it will be removed once GKraken no longer needs it, or
+        # in liquidctl 1.10.x, whatever happens first
+
+        msg = self._read()
+
+        ret = [
+            (_STATUS_TEMPERATURE, msg[1] + msg[2]/10, '°C'),
+            (_STATUS_FAN_SPEED, msg[3] << 8 | msg[4], 'rpm'),
+            (_STATUS_PUMP_SPEED, msg[5] << 8 | msg[6], 'rpm'),
+        ]
+
+        # TODO remove
+        if with_firmware:
+            ret.append((_STATUS_FWVERSION, self.firmware_version, ''))
+
+        return ret
+
+    def _get_status_from_hwmon(self, with_firmware):
+        # target: mainline nzxt-kraken2 driver
+
+        # the firmware version is duplicated here as a temporary migration aid
+        # for GKraken; it will be removed once GKraken no longer needs it, or
+        # in liquidctl 1.10.x, whatever happens first
+
+        ret = [
+            (_STATUS_TEMPERATURE, self._hwmon.get_int('temp1_input') * 1e-3, '°C'),
+            (_STATUS_FAN_SPEED, self._hwmon.get_int('fan1_input'), 'rpm'),
+            (_STATUS_PUMP_SPEED, self._hwmon.get_int('fan2_input'), 'rpm'),
+        ]
+
+        # TODO remove
+        if with_firmware:
+            ret.append((_STATUS_FWVERSION, self.firmware_version, ''))
+
+        return ret
+
+    def get_status(self, direct_access=False, *, _internal_called_from_cli=False, **kwargs):
         """Get a status report.
 
         Returns a list of `(property, value, unit)` tuples.
@@ -138,13 +181,18 @@ class Kraken2(UsbHidDriver):
         if self.device_type == self.DEVICE_KRAKENM:
             return []
 
-        msg = self._read()
+        # already omit the firmware version when the caller is our own cli
+        with_firmware = not _internal_called_from_cli
 
-        return [
-            ('Liquid temperature', msg[1] + msg[2]/10, '°C'),
-            ('Fan speed', msg[3] << 8 | msg[4], 'rpm'),
-            ('Pump speed', msg[5] << 8 | msg[6], 'rpm'),
-        ]
+        if self._hwmon and not direct_access:
+            _LOGGER.info('bound to %s kernel driver, reading status from hwmon', self._hwmon.module)
+            return self._get_status_from_hwmon(with_firmware)
+
+        if self._hwmon:
+            _LOGGER.warning('directly reading the status despite %s kernel driver',
+                            self._hwmon.module)
+
+        return self._get_status_directly(with_firmware)
 
     def set_color(self, channel, mode, colors, speed='normal', direction='forward', **kwargs):
         """Set the color mode for a specific channel."""

liquidctl/cli.py
@@ -415,6 +415,7 @@ def main():
         args['--verbose'] = True
 
     opts = _make_opts(args)
+    opts['_internal_called_from_cli'] = True  # FOR INTERNAL USE ONLY, DO NOT REPLICATE ELSEWHERE
     filter_count = sum(1 for opt in opts if opt in _FILTER_OPTIONS)
     device_id = None
 

tests/test_kraken2.py
@@ -1,6 +1,7 @@
 import pytest
 from _testutils import MockHidapiDevice
 
+from liquidctl.driver.hwmon import HwmonDevice
 from liquidctl.driver.kraken2 import Kraken2
 from liquidctl.error import NotSupportedByDevice
 
@@ -74,12 +75,39 @@ def test_kraken_initialize(mockKrakenXDevice):
     assert fw_ver[1] == '6.0.2'
 
 
-def test_kraken_get_status(mockKrakenXDevice):
-    fan, temp, pump = sorted(mockKrakenXDevice.get_status())
+@pytest.mark.parametrize('has_hwmon,direct_access', [(False, False), (True, True)])
+def test_kraken_get_status_directly(mockKrakenXDevice, has_hwmon, direct_access):
+    if has_hwmon:
+        mockKrakenXDevice._hwmon = HwmonDevice(None, None)
 
-    assert temp[1] == pytest.approx(mockKrakenXDevice.device.temperature)
-    assert fan[1] == mockKrakenXDevice.device.fan_speed
-    assert pump[1] == mockKrakenXDevice.device.pump_speed
+    got = mockKrakenXDevice.get_status(direct_access=direct_access)
+
+    expected = [
+        ('Liquid temperature', pytest.approx(30.9), '°C'),
+        ('Fan speed', 1499, 'rpm'),
+        ('Pump speed', 2702, 'rpm'),
+        ('Firmware version', (6, 0, 2), ''),
+    ]
+
+    assert sorted(got) == sorted(expected)
+
+
+def test_kraken_get_status_from_hwmon(mockKrakenXDevice, tmp_path):
+    mockKrakenXDevice._hwmon = HwmonDevice('mock_module', tmp_path)
+    (tmp_path / 'temp1_input').write_text('20900\n')
+    (tmp_path / 'fan1_input').write_text('2499\n')
+    (tmp_path / 'fan2_input').write_text('1702\n')
+
+    got = mockKrakenXDevice.get_status()
+
+    expected = [
+        ('Liquid temperature', pytest.approx(20.9), '°C'),
+        ('Fan speed', 2499, 'rpm'),
+        ('Pump speed', 1702, 'rpm'),
+        ('Firmware version', (6, 0, 2), ''),
+    ]
+
+    assert sorted(got) == sorted(expected)
 
 
 def test_kraken_not_totally_broken(mockKrakenXDevice):

commit b328b23acb712048070261f8228c1892cbe8a549

kraken2: return the firmware version as a string
Fixes: 5dc22aa26092 ("kraken2: get status from hwmon if possible")

liquidctl/driver/kraken2.py
@@ -151,7 +151,8 @@ class Kraken2(UsbHidDriver):
 
         # TODO remove
         if with_firmware:
-            ret.append((_STATUS_FWVERSION, self.firmware_version, ''))
+            firmware_version = '{}.{}.{}'.format(*self.firmware_version)
+            ret.append((_STATUS_FWVERSION, firmware_version, ''))
 
         return ret
 
@@ -168,7 +169,8 @@ class Kraken2(UsbHidDriver):
 
         # TODO remove
         if with_firmware:
-            ret.append((_STATUS_FWVERSION, self.firmware_version, ''))
+            firmware_version = '{}.{}.{}'.format(*self.firmware_version)
+            ret.append((_STATUS_FWVERSION, firmware_version, ''))
 
         return ret
 