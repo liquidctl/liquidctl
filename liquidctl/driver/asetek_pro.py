liquidctl/liquidctl

commit b86d6c5f2fb8a95b59c1a7a155a34b23e6c46dde

asetek_pro: add driver for Corsair Pro coolers
Continues PR #234, with a substantial number of small fixes and changes.

The tests still need to be ported to pytest, and there are also some
open questions in driver.

liquidctl/driver/asetek_pro.py
@@ -1,13 +1,6 @@
-"""liquidctl drivers for sixth generation Asetek 690LC liquid coolers.
+"""liquidctl drivers for sixth generation Asetek liquid coolers.
 
-Supported devices:
-
-- Corsair H100i PRO RGB, H115i PRO RGB or H150i PRO RGB (Experimental)
-
-Copyright (C) 2018–2020  Jonas Malaco and contributors
-
-Incorporates or uses as reference work by Kristóf Jakab, Sean Nelson
-and Chris Griffith.
+Copyright (C) 2020–2021  Andrew Robertson, Jonas Malaco and contributors
 
 SPDX-License-Identifier: GPL-3.0-or-later
 """
@@ -15,64 +8,56 @@ SPDX-License-Identifier: GPL-3.0-or-later
 import itertools
 import logging
 
-import usb
-
 from liquidctl.driver.asetek import _CommonAsetekDriver
-from liquidctl.driver.usb import UsbDriver
 from liquidctl.error import NotSupportedByDevice
-from liquidctl.keyval import RuntimeStorage
-from liquidctl.util import clamp, LazyHexRepr
-
+from liquidctl.util import clamp
 
 _LOGGER = logging.getLogger(__name__)
 
-_MAX_PROFILE_POINTS = 7
-_CRITICAL_TEMPERATURE = 60
-_HIGH_TEMPERATURE = 45
-_READ_LENGTH = 32
+_READ_ENDPOINT = 0x81
+_READ_MAX_LENGTH = 32
 _READ_TIMEOUT = 2000
+_WRITE_ENDPOINT = 0x1
 _WRITE_TIMEOUT = 2000
 
-_WRITE_ENDPOINT = 0x1
-_READ_ENDPOINT = 0x81
-_CMD_READ_FIRMWARE = 0xaa
+_MAX_PROFILE_POINTS = 7
+
 _CMD_READ_AIO_TEMP = 0xa9
 _CMD_READ_FAN_SPEED = 0x41
+_CMD_READ_FIRMWARE = 0xaa
+_CMD_READ_PUMP_MODE = 0x33
+_CMD_READ_PUMP_SPEED = 0x31
+_CMD_WRITE_COLOR_LIST = 0x56
+_CMD_WRITE_COLOR_SPEED = 0x53
 _CMD_WRITE_FAN_CURVE = 0x40
 _CMD_WRITE_FAN_SPEED = 0x42
-_CMD_READ_PUMP_SPEED = 0x31
-_CMD_READ_PUMP_MODE = 0x33
 _CMD_WRITE_PUMP_MODE = 0x32
-_CMD_WRITE_COLOR_LIST = 0x56
-_PUMP_MODES = [
-    'quiet',
-    'balanced',
-    'performance'
-    ]
+
+_PUMP_MODES = ['quiet', 'balanced', 'performance']
 
 _COLOR_SPEEDS = ['slower', 'normal', 'faster']
 
 _COLOR_SPEEDS_VALUES = {
     'rainbow': [
-        0x30, # slow
-        0x18, # medium
-        0x0C  # fast
+        0x30, # slower
+        0x18, # normal
+        0x0C  # faster
     ],
     'shift': [
-        0x46, # slow
-        0x28, # medium
-        0x0F  # fast
+        0x46, # slower
+        0x28, # normal
+        0x0F  # faster
     ],
     'pulse':[
-        0x50, # slow
-        0x37, # medium
-        0x1E  # fast
+        0x50, # slower
+        0x37, # normal
+        0x1E  # faster
     ],
     'blinking': [
-        0x0F, # slow
-        0x0A, # medium
-        0x05  # fast
-    ]
+        0x0F, # slower
+        0x0A, # normal
+        0x05  # faster
+    ],
 }
 
 _COLOR_CHANGE_MODES = {
@@ -81,180 +66,204 @@ _COLOR_CHANGE_MODES = {
     'shift': [0x55, 0x01],
     'pulse': [0x52, 0x01],
     'blinking': [0x58, 0x01],
-    'fixed': [0x55, 0x01]
+    'fixed': [0x55, 0x01],
+}
+
+# FIXME unknown required and maximum values
+_COLOR_COUNT_BOUNDS = {
+    'rainbow': (0, 0),
+    'alert': (3, 3),
+    'shift': (2, 4),
+    'pulse': (1, 4),
+    'blinking': (1, 4),
+    'fixed': (1, 1),
 }
 
 
+def _quoted(*names):
+    return ', '.join(map(repr, names))
+
+
+# we inherit from _CommonAsetekDriver to reuse its implementation of connect
+# and disconnect, that emulates the stock SiUSBXp driver on Windows
 class CorsairAsetekProDriver(_CommonAsetekDriver):
     """liquidctl driver for Corsair-branded sixth generation Asetek coolers."""
+
     SUPPORTED_DEVICES = [
-        (0x1b1c, 0x0c12, None, 'Corsair Hydro H150i Pro', {'fan_count': 3}),
-        (0x1b1c, 0x0c13, None, 'Corsair Hydro H115i Pro', {'fan_count': 2}),
-        (0x1b1c, 0x0c15, None, 'Corsair Hydro H100i Pro', {'fan_count': 2})
+        (0x1b1c, 0x0c12, None, 'Corsair Hydro H150i Pro (experimental)', {'fan_count': 3}),
+        (0x1b1c, 0x0c13, None, 'Corsair Hydro H115i Pro (experimental)', {'fan_count': 2}),
+        (0x1b1c, 0x0c15, None, 'Corsair Hydro H100i Pro (experimental)', {'fan_count': 2})
     ]
 
-    def __init__(self, device, description, fan_count=0, **kwargs):
+    def __init__(self, device, description, fan_count, **kwargs):
         super().__init__(device, description, **kwargs)
-        self._data = None
         self._fan_count = fan_count
+        self._data = None
 
+    def _post(self, data, *, read_length=None):
+        """Write `data` and return response of up to `read_length` bytes."""
 
-    def _write(self, data):
-        """Write data to the AIO and log"""
+        assert read_length is not None and read_length <= _READ_MAX_LENGTH
+
         self.device.write(_WRITE_ENDPOINT, data, _WRITE_TIMEOUT)
-
-    # Not calling this (or begin transaction) since they do not seem to be needed for H100i Pro
-    def _end_transaction_and_read(self, length=_READ_LENGTH):
-        """End the transaction by reading from the device.
-        According to the official documentation, as well as Craig's open-source
-        implementation (libSiUSBXp), it should be necessary to check the queue
-        size and read data in chunks.  However, leviathan and its derivatives
-        seem to work fine without this complexity; we currently try the same
-        approach.
-        """
-        msg = self.device.read(_READ_ENDPOINT, length, _READ_TIMEOUT)
-        self.device.release()
-        return msg
-
-    def _write_color_change_speed(self, mode, speed):
-        """Send the speed to cycle colors on the RGB pump"""
-        self._write([0x53, _COLOR_SPEEDS_VALUES[mode][_COLOR_SPEEDS.index(speed)]])
-        self._end_transaction_and_read(3)
-
-    def _write_color(self, colors):
-        """Write a list of colors to cycle between, the cycle pattern comes from the
-        value used for cycle speed"""
-        setColors = list(itertools.chain(*colors))
-        colorCount = int(len(setColors) / 3)
-        self._write([_CMD_WRITE_COLOR_LIST, colorCount] + setColors)
-        self._end_transaction_and_read(3)
-
-    def _get_fan_speeds(self):
-        """Read the RPM speed of the fans. Try to get the speeds for 3 fans as that is
-        how many the H150i PRO supports"""
-        speeds = []
-        for i in range(self._fan_count):
-            self._write([_CMD_READ_FAN_SPEED, i])
-            msg = self._end_transaction_and_read(6)
-            if msg[0] != 0x41 or msg[1] != 0x12 or msg[2] != 0x34 or msg[3] != i:
-                _LOGGER.debug('Unable to get speed for fan id %d', i)
-                speeds.append(-1)
-                continue
-            speeds.append((msg[4] << 8) + msg[5])
-        return speeds
-
-    def _get_fan_indexes(self, channel):
-        if len(channel) > 3:
-            channel_num = int(channel[3:])
-            if channel_num > self._fan_count:
-                raise ValueError(f'Unknown channel: {channel}')
-            return [channel_num - 1]
-        return range(self._fan_count)
+        return self.device.read(_READ_ENDPOINT, read_length, _READ_TIMEOUT)[0:read_length]
 
     def initialize(self, pump_mode='balanced', **kwargs):
         """Initialize the device."""
-        _LOGGER.debug('Pro configure device...')
-        _LOGGER.debug('pump_mode %s', pump_mode)
+
         pump_mode = pump_mode.lower()
+
         if pump_mode not in _PUMP_MODES:
-            valid = ", ".join(_PUMP_MODES)
-            raise KeyError(f'Unknown pump mode {pump_mode}, should be one of {valid}')
-        self._write([_CMD_WRITE_PUMP_MODE, _PUMP_MODES.index(pump_mode.lower())])
-        self._end_transaction_and_read(5)
+            raise ValueError(f'unknown pump mode, should be one of: {_quoted(*_PUMP_MODES)}')
+
+        self._post([_CMD_WRITE_PUMP_MODE, _PUMP_MODES.index(pump_mode)], read_length=5)
+        self.device.release()
 
     def get_status(self, **kwargs):
         """Get a status report.
+
         Returns a list of `(property, value, unit)` tuples.
         """
-        # Liquid temperature
-        self._write([_CMD_READ_AIO_TEMP])
-        msg = self._end_transaction_and_read(6)
+
+        msg = self._post([_CMD_READ_AIO_TEMP], read_length=6)
         aio_temp = msg[3] + msg[4]/10
+
         speeds = self._get_fan_speeds()
-        # Pump mode
-        self._write([_CMD_READ_PUMP_MODE])
-        msg = self._end_transaction_and_read(4)
-        pump_mode = msg[3]
-        # Pump Speed
-        self._write([_CMD_READ_PUMP_SPEED])
-        msg = self._end_transaction_and_read(5)
+
+        msg = self._post([_CMD_READ_PUMP_MODE], read_length=4)
+        pump_mode = _PUMP_MODES[msg[3]]
+
+        msg = self._post([_CMD_READ_PUMP_SPEED], read_length=5)
         pump_speed = (msg[3] << 8) + msg[4]
-        # Firmware
-        self._write([_CMD_READ_FIRMWARE])
-        msg = self._end_transaction_and_read(7)
+
+        msg = self._post([_CMD_READ_FIRMWARE], read_length=7)
         firmware = '{}.{}.{}.{}'.format(*tuple(msg[3:7]))
+
+        self.device.release()
+
         status = [('Liquid temperature', aio_temp, '°C')]
-        for i in range(len(speeds)):
-            speed = speeds[i] if speeds[i] >= 0 else 'Error'
-            status.append((f'Fan {i+1} speed', speed, 'rpm'))
+
+        for i, speed in enumerate(speeds):
+            if speed is not None:
+                status.append((f'Fan {i + 1} speed', speed, 'rpm'))
+
         return status + [
-            ('Pump mode', _PUMP_MODES[pump_mode], ""),
+            ('Pump mode', pump_mode, ""),
             ('Pump speed', pump_speed, 'rpm'),
             ('Firmware version', firmware, '')
         ]
 
-    def set_color(self, channel, mode, colors, speed='faster',
-                  temps=['35', '45', '55'], **kwargs):
+    def _get_fan_speeds(self):
+        """Read the RPM speed of the fans."""
+
+        speeds = []
+
+        for i in range(self._fan_count):
+            msg = self._post([_CMD_READ_FAN_SPEED, i], read_length=6)
+
+            if msg[0] != 0x41 or msg[1] != 0x12 or msg[2] != 0x34 or msg[3] != i:
+                _LOGGER.warning('failed to get current speed of fan %d', i)
+                speeds.append(None)
+                continue
+
+            speeds.append((msg[4] << 8) + msg[5])
+        return speeds
+
+    def set_color(self, channel, mode, colors, speed='normal', **kwargs):
         """Set the color mode for a specific channel."""
+
+        mode = mode.lower()
         speed = speed.lower()
-        if mode not in _COLOR_CHANGE_MODES:
-            valid = ', '.join(_COLOR_CHANGE_MODES.keys())
-            raise KeyError(f'Unknown lighting mode {mode}, should be one of {valid}')
-        if speed not in _COLOR_SPEEDS:
-            valid = ', '.join(_COLOR_SPEEDS)
-            raise KeyError(f'Unknown speed {speed}, should be one of {valid}')
-        if channel != 'pump':
-            raise KeyError('Cosair PRO only supports setting the color on the pump')
         colors = list(colors)
-        _LOGGER.debug('color count = %d', len(colors))
-        _LOGGER.debug('color change speed %s', speed)
+
+        if mode not in _COLOR_CHANGE_MODES:
+            valid = _quoted(*_COLOR_CHANGE_MODES.keys())
+            raise ValueError(f'unknown lighting mode, should be one of: {valid}')
+
+        if speed not in _COLOR_SPEEDS:
+            valid = _quoted(*_COLOR_SPEEDS)
+            raise ValueError(f'unknown speed value, should be one of {valid}')
+
         if mode == 'alert':
-            self._write([0x5f, int(temps[0]), 0x00, int(temps[1]), 0x00, int(temps[2]), 0x00] + colors[0] + colors[1] + colors[2])
-            self._end_transaction_and_read(6)
-            self._write([0x5E, 0x01])
-            self._end_transaction_and_read(3)
+            # FIXME this mode is far from being completely implemented; for
+            # one, the temperatures are hardcoded; additionally, it may also be
+            # possible to combine it with other modes, but exploring that would
+            # require some experimentation
+            temps = (35, 45, 55)
+            self._post([0x5f, temps[0], 0x00, temps[1], 0x00, temps[1], 0x00]
+                       + colors[0] + colors[1] + colors[2], read_length=6)
+            self._post([0x5e, 0x01], read_length=3)
+            self.device.release()
             return
-        if mode == 'fixed':
-            colors = [colors[0], colors[0]]
+
+        colors = self._check_color_count_bounds(colors, mode)
+
         if mode != 'rainbow':
-            self._write_color(colors)
+            if mode == 'fixed':
+                colors = [colors[0], colors[0]]
+
+            set_colors = list(itertools.chain(*colors))
+            self._post([_CMD_WRITE_COLOR_LIST, len(colors)] + set_colors, read_length=3)
+
         if mode != 'fixed':
-            self._write_color_change_speed(mode, speed)
-        self._write(_COLOR_CHANGE_MODES[mode])
-        self._end_transaction_and_read(3)
+            magic_value = _COLOR_SPEEDS_VALUES[mode][_COLOR_SPEEDS.index(speed)]
+            self._post([_CMD_WRITE_COLOR_SPEED, magic_value], read_length=3)
+
+        self._post(_COLOR_CHANGE_MODES[mode], read_length=3)
+        self.device.release()
+
+    def _check_color_count_bounds(self, color_list, mode_name):
+        requires, maximum = _COLOR_COUNT_BOUNDS[mode_name]
+
+        if len(color_list) < requires:
+            raise ValueError(f'{mode_name} mode requires {requires} colors')
+
+        if len(color_list) > maximum:
+            _LOGGER.debug('too many colors, dropping to %d', maximum)
+            color_list = color_list[:maximum]
+
+        return color_list
 
     def set_speed_profile(self, channel, profile, **kwargs):
         """Set channel to follow a speed duty profile."""
+
         channel = channel.lower()
-        if channel == 'pump':
-            raise NotSupportedByDevice()
-        if not channel.startswith('fan'):
-            raise ValueError(f'Unknown channel: {channel}')
+        fan_indexes = self._fan_indexes(channel)
+
         adjusted = self._prepare_profile(profile, 0, 100, _MAX_PROFILE_POINTS)
         for temp, duty in adjusted:
             _LOGGER.info('setting %s PWM point: (%i°C, %i%%), device interpolated',
                         channel, temp, duty)
+
         temps, duties = map(list, zip(*adjusted))
-        # Need to write curve for each fan in channel
-        for i in self._get_fan_indexes(channel):
-            _LOGGER.info('setting speed for fan %d', i+1)
-            self._write([_CMD_WRITE_FAN_CURVE, i] + temps + duties)
-            self._end_transaction_and_read(32)
+        for i in fan_indexes:
+            self._post([_CMD_WRITE_FAN_CURVE, i] + temps + duties, read_length=32)
+
+        self.device.release()
 
     def set_fixed_speed(self, channel, duty, **kwargs):
         """Set channel to a fixed speed duty."""
+
         channel = channel.lower()
+        duty = clamp(duty, 0, 100)
+
+        for i in self._fan_indexes(channel):
+            _LOGGER.info('setting speed for fan %d to %d', i + 1, duty)
+            self._post([_CMD_WRITE_FAN_SPEED, i, duty], read_length=32)
+        self.device.release()
+
+    def _fan_indexes(self, channel):
         if channel.startswith('fan'):
-            duty = clamp(duty, 0, 100)
-            # Need to write curve for each fan in channel
-            for i in self._get_fan_indexes(channel):
-                _LOGGER.info('setting speed for fan %d to %d', i+1, duty)
-                self._write([_CMD_WRITE_FAN_SPEED, i, duty])
-                self._end_transaction_and_read(32)
+            if len(channel) > 3:
+                channel_num = int(channel[3:]) - 1
+                if channel_num >= self._fan_count:
+                    raise ValueError(f'unknown channel: {channel}')
+                return [channel_num]
+            return range(self._fan_count)
         elif channel == 'pump':
             raise NotSupportedByDevice()
         else:
-            raise KeyError(f'Unknow channel: {channel}')
+            raise ValueError(f'unknown channel: {channel}')
 
     @classmethod
     def probe(cls, handle, **kwargs):

extra/linux/71-liquidctl.rules
@@ -42,7 +42,7 @@ SUBSYSTEMS=="usb", ATTRS{idVendor}=="2433", ATTRS{idProduct}=="b200", TAG+="uacc
 # Corsair Commander Pro
 SUBSYSTEMS=="usb", ATTRS{idVendor}=="1b1c", ATTRS{idProduct}=="0c10", TAG+="uaccess"
 
-# Corsair H100i PRO
+# Corsair H100i Pro
 SUBSYSTEMS=="usb", ATTRS{idVendor}=="1b1c", ATTRS{idProduct}=="0c15", TAG+="uaccess"
 
 # Corsair H100i Pro XT
@@ -54,7 +54,7 @@ SUBSYSTEMS=="usb", ATTRS{idVendor}=="1b1c", ATTRS{idProduct}=="0c19", TAG+="uacc
 # Corsair H100i Platinum
 SUBSYSTEMS=="usb", ATTRS{idVendor}=="1b1c", ATTRS{idProduct}=="0c18", TAG+="uaccess"
 
-# Corsair H115i PRO
+# Corsair H115i Pro
 SUBSYSTEMS=="usb", ATTRS{idVendor}=="1b1c", ATTRS{idProduct}=="0c13", TAG+= "uaccess"
 
 # Corsair H115i Pro XT
@@ -63,7 +63,7 @@ SUBSYSTEMS=="usb", ATTRS{idVendor}=="1b1c", ATTRS{idProduct}=="0c21", TAG+="uacc
 # Corsair H115i Platinum
 SUBSYSTEMS=="usb", ATTRS{idVendor}=="1b1c", ATTRS{idProduct}=="0c17", TAG+="uaccess"
 
-# Corsair H150i PRO
+# Corsair H150i Pro
 SUBSYSTEMS=="usb", ATTRS{idVendor}=="1b1c", ATTRS{idProduct}=="0c12", TAG+="uaccess"
 
 # Corsair H150i Pro XT

commit 81c7078f7c816d67c7fb7c191ec5455e2308da95

asetek_pro: fix duplicate use of second alert temp (#484)
Somewhere between the initial proposal[1]  and the final inclusion[2] of this feature, this seems to have been changed to use the second alert temperature twice.

This is a bug that results in what I can only describe as the colour getting "stuck" at the interpolated halfway-point between color2 and color3 when temperature rises over 40 degrees.

Tested with my h100i Pro RGB, using all three temperatures results in the expected behaviour of interpolation between color1 and color2 between 30 and 40 degrees, and interpolation between color2 and color3 between 40 and 50 degrees.

[1] <https://github.com/liquidctl/liquidctl/pull/234/files#diff-6e83d451ee55e2e559f9aa43783fe2574adddb8edaf9252aca4b476b5f0821b5R212>
[2] <https://github.com/liquidctl/liquidctl/pull/264/files#diff-6e83d451ee55e2e559f9aa43783fe2574adddb8edaf9252aca4b476b5f0821b5R186>

Fixes: b86d6c5f2fb8a
Related: #461 (possibly fixes it too)

liquidctl/driver/asetek_pro.py
@@ -183,7 +183,7 @@ class HydroPro(_Base690Lc):
             # possible to combine it with other modes, but exploring that would
             # require some experimentation
             temps = (30, 40, 50)
-            self._post([0x5f, temps[0], 0x00, temps[1], 0x00, temps[1], 0x00]
+            self._post([0x5f, temps[0], 0x00, temps[1], 0x00, temps[2], 0x00]
                        + colors[0] + colors[1] + colors[2], read_length=6)
             self._post([0x5e, 0x01], read_length=3)
             self.device.release()