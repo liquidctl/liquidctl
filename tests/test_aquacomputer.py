9f57e1cbfe5d95a456df14021f045c03c61b13b0
Aleksa Savic <savicaleksa83@gmail.com>
2022-07-29 22:27:19 +0200

    Prettify status report in binary format; remove useless fw_version and finish hwmon tests

--- tests/test_aquacomputer.py
+++ tests/test_aquacomputer.py
@@ -6,17 +6,17 @@ from liquidctl.driver.aquacomputer import Aquacomputer
 from liquidctl.error import NotSupportedByDriver
 
 D5NEXT_SAMPLE_STATUS_REPORT = bytes.fromhex(
-    "00030DCB597C00010000006403FF00000051000004DC14000001E0007A98AF0"
-    "0000000FFFF000041A803C169000001481ACAA3465CB804B401F4000000527FFF"
-    "7FFF7FFF7FFF7FFF7FFF7FFF7FFF000000000000000009D27FFF00007FFF01F40"
-    "4B400200026016D006300000004B200D7010207B80000000000098D083A098A08"
-    "3A00060001000000000000000000000000011A24015E27101D4CFFBF"
+    "00030DCB597C00010000006403FF00000051000004DC14000001E0007A98AF000"
+    "00000FFFF000041A803C169000001481ACAA3465CB804B401F4000000527FFF7F"
+    "FF7FFF7FFF7FFF7FFF7FFF7FFF000000000000000009D27FFF00007FFF01F404B"
+    "400200026016D006300000004B200D7010207B80000000000098D083A098A083A"
+    "00060001000000000000000000000000011A24015E27101D4CFFBF"
 )
 
 
 @pytest.fixture
 def mockD5NextDevice():
-    device = _MockD5NextDevice(fw_version=1023)
+    device = _MockD5NextDevice()
     dev = Aquacomputer(
         device,
         "Mock Aquacomputer D5 Next",
@@ -28,9 +28,8 @@ def mockD5NextDevice():
 
 
 class _MockD5NextDevice(MockHidapiDevice):
-    def __init__(self, fw_version):
+    def __init__(self):
         super().__init__(vendor_id=0x0C70, product_id=0xF00E)
-        self.fw_version = fw_version
 
         self.preload_read(Report(1, D5NEXT_SAMPLE_STATUS_REPORT))
 
@@ -87,19 +86,34 @@ def test_d5next_get_status_directly(mockD5NextDevice, has_hwmon, direct_access):
 
 
 def test_d5next_get_status_from_hwmon(mockD5NextDevice, tmp_path):
-    # TODO
 
     mockD5NextDevice._hwmon = HwmonDevice("mock_module", tmp_path)
-    (tmp_path / "temp1_input").write_text("20900\n")
-    (tmp_path / "fan1_input").write_text("2499\n")
-    (tmp_path / "fan2_input").write_text("1702\n")
+    (tmp_path / "temp1_input").write_text("25100\n")  # Liquid temperature
+    (tmp_path / "fan1_input").write_text("1976\n")  # Pump speed
+    (tmp_path / "power1_input").write_text("2580000\n")  # Pump power
+    (tmp_path / "in0_input").write_text("12020\n")  # Pump voltage
+    (tmp_path / "curr1_input").write_text("215\n")  # Pump current
+    (tmp_path / "fan2_input").write_text("365\n")  # Fan speed
+    (tmp_path / "power2_input").write_text("380000\n")  # Fan power
+    (tmp_path / "in1_input").write_text("12040\n")  # Fan voltage
+    (tmp_path / "curr2_input").write_text("31\n")  # Fan current
+    (tmp_path / "in2_input").write_text("4990\n")  # +5V voltage
+    (tmp_path / "in3_input").write_text("12040\n")  # +12V voltage
 
     got = mockD5NextDevice.get_status()
 
     expected = [
-        ("Liquid temperature", pytest.approx(20.9), "°C"),
-        ("Fan speed", 2499, "rpm"),
-        ("Pump speed", 1702, "rpm"),
+        ("Liquid temperature", pytest.approx(25.1, 0.1), "°C"),
+        ("Pump speed", 1976, "rpm"),
+        ("Pump power", pytest.approx(2.58, 0.1), "W"),
+        ("Pump voltage", pytest.approx(12.02, 0.1), "V"),
+        ("Pump current", pytest.approx(0.21, 0.1), "A"),
+        ("Fan speed", 365, "rpm"),
+        ("Fan power", pytest.approx(0.38, 0.1), "W"),
+        ("Fan voltage", pytest.approx(12.04, 0.1), "V"),
+        ("Fan current", pytest.approx(0.03, 0.1), "A"),
+        ("+5V voltage", pytest.approx(5.00, 0.1), "V"),
+        ("+12V voltage", pytest.approx(12.04, 0.1), "V"),
     ]
 
     assert sorted(got) == sorted(expected)
