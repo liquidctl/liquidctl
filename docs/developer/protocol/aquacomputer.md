72968d5a5d7588895f64ac1e2c0d59bde2665dfb
Aleksa Savić <savicaleksa83@gmail.com>
2022-08-11 20:15:54 +0200

    aquacomputer: add Octo support (#492)

--- docs/developer/protocol/aquacomputer.md
+++ docs/developer/protocol/aquacomputer.md
@@ -63,3 +63,38 @@ An example sensor report of the Farbwerk 360 looks like this:
 ```
 
 Its ID is `0x01` and its length is `0xb6`. The four temp sensor values are located in succession at `0x32`, `0x34`, `0x36`, `0x38`.
+
+## Octo
+
+The Octo exposes four temperature sensors and eight groups of fan sensor data (outlined in the preamble) through its sensor report.
+
+### Sensor report
+
+An example sensor report of the Octo looks like this:
+
+```
+01 00 02 3A 92 C9 EA 03 E8 00 01 00 65 03 FB 00 00 00 01 00 00 00 48 8E 00 00 00 C2 00 3A DF 11 01 00 01 00 00 00 00 00 00 00 00 00 00 00 00 00 04 9D 84 FF DB FF DC FF DD A7 B0 5B FC 10 17 7F FF 7F FF 7F FF 0F 00 7F FF 7F FF 7F FF 7F FF 7F FF 7F FF 7F FF 7F FF 7F FF 7F FF 7F FF 7F FF 7F FF 7F FF 7F FF 03 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 04 B9 00 02 00 02 00 00 05 5D 04 B9 00 00 00 00 00 00 00 00 08 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 21 3B 04 B9 00 02 00 02 00 00 00 00 08 00 00 00 00 03 E8 05 5D 00 00 00 00 03 E8 00 00 00 00 00 00 03 E8 00 00 00 00 00 00 03 E8 00 00 00 00 00 00 03 E8 00 00 00 00 00 00 03 E8 00 00 00 00 00 00 03 E8 00 00 00 00 00 00 03 E8 21 3B 00 00 00 00 03 E8 27 10 00 00 00 00 03 E8 27 10 00 00 00 00 00 00 00 00 15 B3 15 0A 27 10 27 10 FF FF
+```
+
+Its ID is `0x01` and its length is `0x147`.
+
+Here is what it's currently known to contain:
+
+| What                               | Where/starts at (offset) |
+|------------------------------------|--------------------------|
+| Serial number (first part)         | 0x03                     |
+| Serial number (second part)        | 0x05                     |
+| Firmware version                   | 0xD                      |
+| Number of power cycles *[4 bytes]* | 0x18                     |
+| Temp sensor 1                      | 0x3D                     |
+| Temp sensor 2                      | 0x3F                     |
+| Temp sensor 3                      | 0x41                     |
+| Temp sensor 4                      | 0x43                     |
+| Fan 1 substructure                 | 0x7D                     |
+| Fan 2 substructure                 | 0x8A                     |
+| Fan 3 substructure                 | 0x97                     |
+| Fan 4 substructure                 | 0xA4                     |
+| Fan 5 substructure                 | 0xB1                     |
+| Fan 6 substructure                 | 0xBE                     |
+| Fan 7 substructure                 | 0xCB                     |
+| Fan 8 substructure                 | 0xD8                     |
--- tests/test_aquacomputer.py
+++ tests/test_aquacomputer.py
@@ -22,6 +22,20 @@ FARBWERK360_SAMPLE_STATUS_REPORT = bytes.fromhex(
     "0000000000000000000010002000101040006"
 )
 
+OCTO_SAMPLE_STATUS_REPORT = bytes.fromhex(
+    "00023A92C9EA03E80001006503FB000000010000010DB4000000C5003C3EA4010"
+    "00200000000000000000000000000059EDCFFDCFFDDFFDDA7A65BF80AC60ACF0B"
+    "150D600EC87FFF7FFF7FFF7FFF7FFF7FFF7FFF7FFF7FFF7FFF7FFF7FFF7FFF7FF"
+    "F7FFF0300000000000000000000000000000004B9000300030000055D04B90001"
+    "00010000000008138804B9015E006702400000000000000000000000000000000"
+    "00000000000000000000000000000000000000000000000000000000000000000"
+    "0000000000000000000000000000000000000000213B04B900020002000000000"
+    "80000000003E8055D0000000003E800000000000003E800000000000003E80000"
+    "0000000003E800000000000003E800000000000003E800000000000003E8213B0"
+    "000000003E827100000000003E827100000000000000000120412862710271098"
+    "20"
+)
+
 
 @pytest.fixture
 def mockD5NextDevice():
@@ -240,3 +254,197 @@ def test_farbwerk360_set_fixed_speeds_not_supported(mockFarbwerk360Device):
 def test_farbwerk360_speed_profiles_not_supported(mockFarbwerk360Device):
     with pytest.raises(NotSupportedByDevice):
         mockFarbwerk360Device.set_speed_profile("fan", None)
+
+
+@pytest.fixture
+def mockOctoDevice():
+    device = _MockOctoDevice()
+    dev = Aquacomputer(
+        device,
+        "Mock Aquacomputer Octo",
+        device_info=Aquacomputer._DEVICE_INFO[Aquacomputer._DEVICE_OCTO],
+    )
+
+    dev.connect()
+    return dev
+
+
+class _MockOctoDevice(MockHidapiDevice):
+    def __init__(self):
+        super().__init__(vendor_id=0x0C70, product_id=0xF011)
+
+        self.preload_read(Report(1, OCTO_SAMPLE_STATUS_REPORT))
+
+    def read(self, length):
+        pre = super().read(length)
+        if pre:
+            return pre
+
+        return Report(1, OCTO_SAMPLE_STATUS_REPORT)
+
+
+def test_octo_connect(mockOctoDevice):
+    def mock_open():
+        nonlocal opened
+        opened = True
+
+    mockOctoDevice.device.open = mock_open
+    opened = False
+
+    with mockOctoDevice.connect() as cm:
+        assert cm == mockOctoDevice
+        assert opened
+
+
+def test_octo_initialize(mockOctoDevice):
+    init_result = mockOctoDevice.initialize()
+
+    # Verify firmware version
+    assert init_result[0][1] == 1019
+
+    # Verify serial number
+    assert init_result[1][1] == "14994-51690"
+
+
+@pytest.mark.parametrize("has_hwmon,direct_access", [(False, False), (True, True)])
+def test_octo_get_status_directly(mockOctoDevice, has_hwmon, direct_access):
+    if has_hwmon:
+        mockOctoDevice._hwmon = HwmonDevice(None, None)
+
+    got = mockOctoDevice.get_status(direct_access=direct_access)
+
+    print(f"octo: got: {got}")
+
+    expected = [
+        ("Sensor 1", pytest.approx(27.5, 0.1), "°C"),
+        ("Sensor 2", pytest.approx(27.7, 0.1), "°C"),
+        ("Sensor 3", pytest.approx(28.4, 0.1), "°C"),
+        ("Sensor 4", pytest.approx(34.2, 0.1), "°C"),
+        ("Fan 1 speed", pytest.approx(0, 0.1), "rpm"),
+        ("Fan 1 power", pytest.approx(0.01, 0.1), "W"),
+        ("Fan 1 voltage", pytest.approx(12.09, 0.1), "V"),
+        ("Fan 1 current", pytest.approx(0.001, 0.1), "A"),
+        ("Fan 2 speed", pytest.approx(576, 0.1), "rpm"),
+        ("Fan 2 power", pytest.approx(1.03, 0.1), "W"),
+        ("Fan 2 voltage", pytest.approx(12.09, 0.1), "V"),
+        ("Fan 2 current", pytest.approx(0.35, 0.1), "A"),
+        ("Fan 3 speed", pytest.approx(0, 0.1), "rpm"),
+        ("Fan 3 power", pytest.approx(0, 0.1), "W"),
+        ("Fan 3 voltage", pytest.approx(0, 0.1), "V"),
+        ("Fan 3 current", pytest.approx(0, 0.1), "A"),
+        ("Fan 4 speed", pytest.approx(0, 0.1), "rpm"),
+        ("Fan 4 power", pytest.approx(0, 0.1), "W"),
+        ("Fan 4 voltage", pytest.approx(0, 0.1), "V"),
+        ("Fan 4 current", pytest.approx(0, 0.1), "A"),
+        ("Fan 5 speed", pytest.approx(0, 0.1), "rpm"),
+        ("Fan 5 power", pytest.approx(0, 0.1), "W"),
+        ("Fan 5 voltage", pytest.approx(0, 0.1), "V"),
+        ("Fan 5 current", pytest.approx(0, 0.1), "A"),
+        ("Fan 6 speed", pytest.approx(0, 0.1), "rpm"),
+        ("Fan 6 power", pytest.approx(0, 0.1), "W"),
+        ("Fan 6 voltage", pytest.approx(0, 0.1), "V"),
+        ("Fan 6 current", pytest.approx(0, 0.1), "A"),
+        ("Fan 7 speed", pytest.approx(0, 0.1), "rpm"),
+        ("Fan 7 power", pytest.approx(0, 0.1), "W"),
+        ("Fan 7 voltage", pytest.approx(0, 0.1), "V"),
+        ("Fan 7 current", pytest.approx(0, 0.1), "A"),
+        ("Fan 8 speed", pytest.approx(0, 0.1), "rpm"),
+        ("Fan 8 power", pytest.approx(0.02, 0.1), "W"),
+        ("Fan 8 voltage", pytest.approx(12.09, 0.1), "V"),
+        ("Fan 8 current", pytest.approx(0.002, 0.1), "A"),
+    ]
+
+    assert sorted(got) == sorted(expected)
+
+
+def test_octo_get_status_from_hwmon(mockOctoDevice, tmp_path):
+    mockOctoDevice._hwmon = HwmonDevice("mock_module", tmp_path)
+    (tmp_path / "temp1_input").write_text("27580\n")
+    (tmp_path / "temp2_input").write_text("27670\n")
+    (tmp_path / "temp3_input").write_text("28370\n")
+    (tmp_path / "temp4_input").write_text("34240\n")
+    (tmp_path / "fan1_input").write_text("0\n")
+    (tmp_path / "power1_input").write_text("10000\n")
+    (tmp_path / "in0_input").write_text("12090\n")
+    (tmp_path / "curr1_input").write_text("1\n")
+    (tmp_path / "fan2_input").write_text("576\n")
+    (tmp_path / "power2_input").write_text("1030000\n")
+    (tmp_path / "in1_input").write_text("12090\n")
+    (tmp_path / "curr2_input").write_text("350\n")
+    (tmp_path / "fan3_input").write_text("0\n")
+    (tmp_path / "power3_input").write_text("0\n")
+    (tmp_path / "in2_input").write_text("0\n")
+    (tmp_path / "curr3_input").write_text("0\n")
+    (tmp_path / "fan4_input").write_text("0\n")
+    (tmp_path / "power4_input").write_text("0\n")
+    (tmp_path / "in3_input").write_text("0\n")
+    (tmp_path / "curr4_input").write_text("0\n")
+    (tmp_path / "fan5_input").write_text("0\n")
+    (tmp_path / "power5_input").write_text("0\n")
+    (tmp_path / "in4_input").write_text("0\n")
+    (tmp_path / "curr5_input").write_text("0\n")
+    (tmp_path / "fan6_input").write_text("0\n")
+    (tmp_path / "power6_input").write_text("0\n")
+    (tmp_path / "in5_input").write_text("0\n")
+    (tmp_path / "curr6_input").write_text("0\n")
+    (tmp_path / "fan7_input").write_text("0\n")
+    (tmp_path / "power7_input").write_text("0\n")
+    (tmp_path / "in6_input").write_text("0\n")
+    (tmp_path / "curr7_input").write_text("0\n")
+    (tmp_path / "fan8_input").write_text("0\n")
+    (tmp_path / "power8_input").write_text("20000\n")
+    (tmp_path / "in7_input").write_text("12090\n")
+    (tmp_path / "curr8_input").write_text("2\n")
+
+    got = mockOctoDevice.get_status()
+
+    expected = [
+        ("Sensor 1", pytest.approx(27.5, 0.1), "°C"),
+        ("Sensor 2", pytest.approx(27.7, 0.1), "°C"),
+        ("Sensor 3", pytest.approx(28.4, 0.1), "°C"),
+        ("Sensor 4", pytest.approx(34.2, 0.1), "°C"),
+        ("Fan 1 speed", pytest.approx(0, 0.1), "rpm"),
+        ("Fan 1 power", pytest.approx(0.01, 0.1), "W"),
+        ("Fan 1 voltage", pytest.approx(12.09, 0.1), "V"),
+        ("Fan 1 current", pytest.approx(0.001, 0.1), "A"),
+        ("Fan 2 speed", pytest.approx(576, 0.1), "rpm"),
+        ("Fan 2 power", pytest.approx(1.03, 0.1), "W"),
+        ("Fan 2 voltage", pytest.approx(12.09, 0.1), "V"),
+        ("Fan 2 current", pytest.approx(0.35, 0.1), "A"),
+        ("Fan 3 speed", pytest.approx(0, 0.1), "rpm"),
+        ("Fan 3 power", pytest.approx(0, 0.1), "W"),
+        ("Fan 3 voltage", pytest.approx(0, 0.1), "V"),
+        ("Fan 3 current", pytest.approx(0, 0.1), "A"),
+        ("Fan 4 speed", pytest.approx(0, 0.1), "rpm"),
+        ("Fan 4 power", pytest.approx(0, 0.1), "W"),
+        ("Fan 4 voltage", pytest.approx(0, 0.1), "V"),
+        ("Fan 4 current", pytest.approx(0, 0.1), "A"),
+        ("Fan 5 speed", pytest.approx(0, 0.1), "rpm"),
+        ("Fan 5 power", pytest.approx(0, 0.1), "W"),
+        ("Fan 5 voltage", pytest.approx(0, 0.1), "V"),
+        ("Fan 5 current", pytest.approx(0, 0.1), "A"),
+        ("Fan 6 speed", pytest.approx(0, 0.1), "rpm"),
+        ("Fan 6 power", pytest.approx(0, 0.1), "W"),
+        ("Fan 6 voltage", pytest.approx(0, 0.1), "V"),
+        ("Fan 6 current", pytest.approx(0, 0.1), "A"),
+        ("Fan 7 speed", pytest.approx(0, 0.1), "rpm"),
+        ("Fan 7 power", pytest.approx(0, 0.1), "W"),
+        ("Fan 7 voltage", pytest.approx(0, 0.1), "V"),
+        ("Fan 7 current", pytest.approx(0, 0.1), "A"),
+        ("Fan 8 speed", pytest.approx(0, 0.1), "rpm"),
+        ("Fan 8 power", pytest.approx(0.02, 0.1), "W"),
+        ("Fan 8 voltage", pytest.approx(12.09, 0.1), "V"),
+        ("Fan 8 current", pytest.approx(0.002, 0.1), "A"),
+    ]
+
+    assert sorted(got) == sorted(expected)
+
+
+def test_octo_set_fixed_speeds_not_supported(mockOctoDevice):
+    with pytest.raises(NotSupportedByDriver):
+        mockOctoDevice.set_fixed_speed("fan", 42)
+
+
+def test_octo_speed_profiles_not_supported(mockOctoDevice):
+    with pytest.raises(NotSupportedByDriver):
+        mockOctoDevice.set_speed_profile("fan", None)
