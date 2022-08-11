liquidctl/liquidctl

commit f749c94809859545479589b7bd44011f2f8d8f6b

corsair_hid_psu: rename temperature sensors according to their location
The association from sensors to corresponding components is taken from
the corsair-psu Linxu hwmon driver.[^1]

A grep-backward-compatible alternative was considered

    Temperature <n> -> Temperature <n> (<location>)

... but would result in very long names.  Additionally, the specific
items returned from get_status() are *not* stable.[^2][^3]

[^1]: https://www.kernel.org/doc/html/v5.11/hwmon/corsair-psu.html
[^2]: docs/developer/process.md#stability-and-backward-compatibility

liquidctl/driver/corsair_hid_psu.py
@@ -159,8 +159,8 @@ class CorsairHidPsu(UsbHidDriver):
         ret = [
             ('Current uptime', self._get_timedelta(_CORSAIR_READ_UPTIME), ''),
             ('Total uptime', self._get_timedelta(_CORSAIR_READ_TOTAL_UPTIME), ''),
-            ('Temperature 1', self._get_float(CMD.READ_TEMPERATURE_1), '°C'),
-            ('Temperature 2', self._get_float(CMD.READ_TEMPERATURE_2), '°C'),
+            ('Voltage regulator temperature', self._get_float(CMD.READ_TEMPERATURE_1), '°C'),
+            ('Case temperature', self._get_float(CMD.READ_TEMPERATURE_2), '°C'),
             ('Fan control mode', self._get_fan_control_mode(), ''),
             ('Fan speed', self._get_float(CMD.READ_FAN_SPEED_1), 'rpm'),
             ('Input voltage', input_voltage, 'V'),
@@ -195,8 +195,8 @@ class CorsairHidPsu(UsbHidDriver):
         input_voltage = self._hwmon.get_int('in0_input') * 1e-3
 
         ret = [
-            ('Temperature 1', self._hwmon.get_int('temp1_input') * 1e-3, '°C'),
-            ('Temperature 2', self._hwmon.get_int('temp2_input') * 1e-3, '°C'),
+            ('Voltage regulator temperature', self._hwmon.get_int('temp1_input') * 1e-3, '°C'),
+            ('Case temperature', self._hwmon.get_int('temp2_input') * 1e-3, '°C'),
             ('Fan speed', self._hwmon.get_int('fan1_input'), 'rpm'),
             ('Input voltage', input_voltage, 'V'),
         ]

tests/test_corsair_hid_psu.py
@@ -128,8 +128,8 @@ def test_reads_status_directly(mock_psu, has_hwmon, direct_access):
     expected = [
         ('Current uptime', timedelta(seconds=5522), ''),
         ('Total uptime', timedelta(days=13, seconds=9122), ''),
-        ('Temperature 1', approx(33.5, rel=1e-3), '°C'),
-        ('Temperature 2', approx(26.5, rel=1e-3), '°C'),
+        ('Voltage regulator temperature', approx(33.5, rel=1e-3), '°C'),
+        ('Case temperature', approx(26.5, rel=1e-3), '°C'),
         ('Fan control mode', FanControlMode.SOFTWARE, ''),
         ('Fan speed', approx(968, rel=1e-3), 'rpm'),
         ('Input voltage', approx(230, rel=1e-3), 'V'),
@@ -173,8 +173,8 @@ def test_reads_status_from_hwmon(mock_psu, tmp_path):
     got = mock_psu.get_status()
 
     expected = [
-        ('Temperature 1', approx(33.5, rel=1e-3), '°C'),
-        ('Temperature 2', approx(26.5, rel=1e-3), '°C'),
+        ('Voltage regulator temperature', approx(33.5, rel=1e-3), '°C'),
+        ('Case temperature', approx(26.5, rel=1e-3), '°C'),
         ('Fan speed', approx(968, rel=1e-3), 'rpm'),
         ('Input voltage', approx(230, rel=1e-3), 'V'),
         ('Total power output', approx(140, rel=1e-3), 'W'),

commit c4525f84d91b28a083a3c50edf1dd190ebc931c2

corsair_hid_psu: fix new temperature sensor names
Commit f749c9480985 ended up with experimental names in the code, and
the final names only in the documentation.  Make the code use the final
names as well.

Fixes: f749c9480985

liquidctl/driver/corsair_hid_psu.py
@@ -159,7 +159,7 @@ class CorsairHidPsu(UsbHidDriver):
         ret = [
             ('Current uptime', self._get_timedelta(_CORSAIR_READ_UPTIME), ''),
             ('Total uptime', self._get_timedelta(_CORSAIR_READ_TOTAL_UPTIME), ''),
-            ('Voltage regulator temperature', self._get_float(CMD.READ_TEMPERATURE_1), '°C'),
+            ('VRM temperature', self._get_float(CMD.READ_TEMPERATURE_1), '°C'),
             ('Case temperature', self._get_float(CMD.READ_TEMPERATURE_2), '°C'),
             ('Fan control mode', self._get_fan_control_mode(), ''),
             ('Fan speed', self._get_float(CMD.READ_FAN_SPEED_1), 'rpm'),
@@ -195,7 +195,7 @@ class CorsairHidPsu(UsbHidDriver):
         input_voltage = self._hwmon.get_int('in0_input') * 1e-3
 
         ret = [
-            ('Voltage regulator temperature', self._hwmon.get_int('temp1_input') * 1e-3, '°C'),
+            ('VRM temperature', self._hwmon.get_int('temp1_input') * 1e-3, '°C'),
             ('Case temperature', self._hwmon.get_int('temp2_input') * 1e-3, '°C'),
             ('Fan speed', self._hwmon.get_int('fan1_input'), 'rpm'),
             ('Input voltage', input_voltage, 'V'),

tests/test_corsair_hid_psu.py
@@ -128,7 +128,7 @@ def test_reads_status_directly(mock_psu, has_hwmon, direct_access):
     expected = [
         ('Current uptime', timedelta(seconds=5522), ''),
         ('Total uptime', timedelta(days=13, seconds=9122), ''),
-        ('Voltage regulator temperature', approx(33.5, rel=1e-3), '°C'),
+        ('VRM temperature', approx(33.5, rel=1e-3), '°C'),
         ('Case temperature', approx(26.5, rel=1e-3), '°C'),
         ('Fan control mode', FanControlMode.SOFTWARE, ''),
         ('Fan speed', approx(968, rel=1e-3), 'rpm'),
@@ -173,7 +173,7 @@ def test_reads_status_from_hwmon(mock_psu, tmp_path):
     got = mock_psu.get_status()
 
     expected = [
-        ('Voltage regulator temperature', approx(33.5, rel=1e-3), '°C'),
+        ('VRM temperature', approx(33.5, rel=1e-3), '°C'),
         ('Case temperature', approx(26.5, rel=1e-3), '°C'),
         ('Fan speed', approx(968, rel=1e-3), 'rpm'),
         ('Input voltage', approx(230, rel=1e-3), 'V'),