liquidctl/liquidctl

commit 3e7cf3b18b967d97bd77feff52250f4e4b937228

smart_device: register new Fan & RGB Controller PID
While RGB control hasn't been tested, it is assumed to behave as in the
previous variants.  This is supported by a similarly timed new variant
of the Kraken X3 being fully backward compatible (see #503).

Fixes: #485
Suggested-by: @johandc

extra/linux/71-liquidctl.rules
@@ -491,6 +491,9 @@ SUBSYSTEMS=="usb", ATTRS{idVendor}=="1e71", ATTRS{idProduct}=="200e", TAG+="uacc
 # NZXT RGB & Fan Controller
 SUBSYSTEMS=="usb", ATTRS{idVendor}=="1e71", ATTRS{idProduct}=="2010", TAG+="uaccess"
 
+# NZXT RGB & Fan Controller
+SUBSYSTEMS=="usb", ATTRS{idVendor}=="1e71", ATTRS{idProduct}=="2019", TAG+="uaccess"
+
 # NZXT Smart Device (V1)
 SUBSYSTEMS=="usb", ATTRS{idVendor}=="1e71", ATTRS{idProduct}=="1714", TAG+="uaccess"
 

liquidctl/driver/smart_device.py
@@ -428,6 +428,10 @@ class SmartDevice2(_BaseSmartDevice):
             'speed_channel_count': 3,
             'color_channel_count': 2
         }),
+        (0x1e71, 0x2019, 'NZXT RGB & Fan Controller', {
+            'speed_channel_count': 3,
+            'color_channel_count': 2
+        }),
     ]
 
     _MAX_READ_ATTEMPTS = 12

commit 361c57674e59e5ca3fb9f435edf2a2427c1e4922

smart_device: enable additional lighting channels in 1e71:2019 controller
Fixes: 3e7cf3b18b96
Related: #485
Related: #541

extra/linux/71-liquidctl.rules
@@ -500,11 +500,11 @@ SUBSYSTEMS=="usb", ATTRS{idVendor}=="1e71", ATTRS{idProduct}=="200e", TAG+="uacc
 # NZXT RGB & Fan Controller
 SUBSYSTEMS=="usb", ATTRS{idVendor}=="1e71", ATTRS{idProduct}=="2010", TAG+="uaccess"
 
-# NZXT RGB & Fan Controller
-SUBSYSTEMS=="usb", ATTRS{idVendor}=="1e71", ATTRS{idProduct}=="2019", TAG+="uaccess"
-
 # NZXT RGB & Fan Controller (3+6 channels)
 SUBSYSTEMS=="usb", ATTRS{idVendor}=="1e71", ATTRS{idProduct}=="2011", TAG+="uaccess"
+
+# NZXT RGB & Fan Controller (3+6 channels)
+SUBSYSTEMS=="usb", ATTRS{idVendor}=="1e71", ATTRS{idProduct}=="2019", TAG+="uaccess"
 
 # NZXT Smart Device (V1)
 SUBSYSTEMS=="usb", ATTRS{idVendor}=="1e71", ATTRS{idProduct}=="1714", TAG+="uaccess"

liquidctl/driver/smart_device.py
@@ -432,9 +432,9 @@ class SmartDevice2(_BaseSmartDevice):
             'speed_channel_count': 3,
             'color_channel_count': 6
         }),
-        (0x1e71, 0x2019, 'NZXT RGB & Fan Controller', {
+        (0x1e71, 0x2019, 'NZXT RGB & Fan Controller (3+6 channels)', {
             'speed_channel_count': 3,
-            'color_channel_count': 2
+            'color_channel_count': 6
         }),
     ]
 
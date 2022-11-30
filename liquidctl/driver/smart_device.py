361c57674e59e5ca3fb9f435edf2a2427c1e4922
Jonas Malaco <jonas@protocubo.io>
2022-11-30 11:30:00 -0300

    smart_device: enable additional lighting channels in 1e71:2019 controller
    Fixes: 3e7cf3b18b96
    Related: #485
    Related: #541

--- liquidctl/driver/smart_device.py
+++ liquidctl/driver/smart_device.py
@@ -432,9 +432,9 @@ class SmartDevice2(_BaseSmartDevice):
             'speed_channel_count': 3,
             'color_channel_count': 6
         }),
-        (0x1e71, 0x2019, 'NZXT RGB & Fan Controller', {
+        (0x1e71, 0x2019, 'NZXT RGB & Fan Controller (3+6 channels)', {
             'speed_channel_count': 3,
-            'color_channel_count': 2
+            'color_channel_count': 6
         }),
     ]
 
