52f365bbe02c364b62bd8d9cd4ac45572fdbc8f4
Serphentas <Serphentas@users.noreply.github.com>
2023-11-15 16:39:35 +0100

    make linter happy

--- liquidctl/driver/lianli_uni.py
+++ liquidctl/driver/lianli_uni.py
@@ -4,49 +4,48 @@ _SYNC_ARGB_COMMAND_LENGTH = 7
 _SYNC_CHANNEL_COMMAND_LENGTH = 4
 
 _SYNC_ARGB_COMMANDS = {
-    'SL': [224, 16, 48, 0, 0, 0],
-    'AL': [224, 16, 65, 0, 0, 0],
-    'SL-Infinity': [224, 16, 97, 0, 0, 0],
-    'SL v2': [224, 16, 97, 0, 0, 0],
-    'AL v2': [224, 16, 97, 0, 0, 0],
+    "SL": [224, 16, 48, 0, 0, 0],
+    "AL": [224, 16, 65, 0, 0, 0],
+    "SL-Infinity": [224, 16, 97, 0, 0, 0],
+    "SL v2": [224, 16, 97, 0, 0, 0],
+    "AL v2": [224, 16, 97, 0, 0, 0],
 }
 _SYNC_CHANNEL_COMMANDS = {
-    'SL': [224, 16, 49],
-    'AL': [224, 16, 66],
-    'SL-Infinity': [224, 16, 98],
-    'SL v2': [224, 16, 98],
-    'AL v2': [224, 16, 98],
+    "SL": [224, 16, 49],
+    "AL": [224, 16, 66],
+    "SL-Infinity": [224, 16, 98],
+    "SL v2": [224, 16, 98],
+    "AL v2": [224, 16, 98],
 }
 
 _PIDS = {
-    'SL': [0x7750, 0xa100],
-    'AL': [0xa101],
-    'SL-Infinity': [0xa102],
-    'SL v2': [0xa103, 0xa105],
-    'AL v2': [0xa104],
+    "SL": [0x7750, 0xA100],
+    "AL": [0xA101],
+    "SL-Infinity": [0xA102],
+    "SL v2": [0xA103, 0xA105],
+    "AL v2": [0xA104],
 }
 
 _CHANNEL_BYTE_MASK = 0x10
 _CHANNEL_PWM_MASK = 0x1
 
+
 class LianLiUNI(UsbHidDriver):
     """Lian-Li Uni fans"""
 
     _MATCHES = [
-        (0x0cf2, 0x7750, 'LianLi-UNI SL',
-            {'fan_count': 6, 'temp_probs': 0, 'led_channels': 2}),
-        (0x0cf2, 0xa100, 'LianLi-UNI SL',
-            {'fan_count': 6, 'temp_probs': 0, 'led_channels': 2}),
-        (0x0cf2, 0xa101, 'LianLi-UNI AL',
-            {'fan_count': 6, 'temp_probs': 0, 'led_channels': 2}),
-        (0x0cf2, 0xa102, 'LianLi-UNI SL-Infinity',
-            {'fan_count': 6, 'temp_probs': 0, 'led_channels': 2}),
-        (0x0cf2, 0xa103, 'LianLi-UNI SL v2',
-            {'fan_count': 6, 'temp_probs': 0, 'led_channels': 2}),
-        (0x0cf2, 0xa105, 'LianLi-UNI SL v2',
-            {'fan_count': 6, 'temp_probs': 0, 'led_channels': 2}),
-        (0x0cf2, 0xa104, 'LianLi-UNI AL v2',
-            {'fan_count': 6, 'temp_probs': 0, 'led_channels': 2}),
+        (0x0CF2, 0x7750, "LianLi-UNI SL", {"fan_count": 4, "temp_probs": 0, "led_channels": 2}),
+        (0x0CF2, 0xA100, "LianLi-UNI SL", {"fan_count": 4, "temp_probs": 0, "led_channels": 2}),
+        (0x0CF2, 0xA101, "LianLi-UNI AL", {"fan_count": 4, "temp_probs": 0, "led_channels": 2}),
+        (
+            0x0CF2,
+            0xA102,
+            "LianLi-UNI SL-Infinity",
+            {"fan_count": 4, "temp_probs": 0, "led_channels": 2},
+        ),
+        (0x0CF2, 0xA103, "LianLi-UNI SL v2", {"fan_count": 4, "temp_probs": 0, "led_channels": 2}),
+        (0x0CF2, 0xA105, "LianLi-UNI SL v2", {"fan_count": 4, "temp_probs": 0, "led_channels": 2}),
+        (0x0CF2, 0xA104, "LianLi-UNI AL v2", {"fan_count": 4, "temp_probs": 0, "led_channels": 2}),
     ]
 
     def __init__(self, device, description, fan_count, temp_probs, led_channels, **kwargs):
@@ -71,22 +70,21 @@ class LianLiUNI(UsbHidDriver):
 
         self.device.write(_SYNC_CHANNEL_COMMANDS[self.variant] + channel_byte)
 
-
     def set_channel_speed(self, channel, speed):
         speed = min(speed, 100.0)
         command = [224, channel + 32, 0]
 
-        if self.variant in ['SL', 'AL']:
+        if self.variant in ["SL", "AL"]:
             command.append((800.0 + (11.0 * speed)) / 19)
-        elif self.variant == 'SL-Infinity':
+        elif self.variant == "SL-Infinity":
             command.append((250.0 + (17.5 * speed)) / 20)
-        elif self.variant in ['SL v2', 'AL v2']:
+        elif self.variant in ["SL v2", "AL v2"]:
             command.append((200.0 + (19.0 * speed)) / 21)
 
         self.device.write(command)
 
     def set_channel_profile(self, channel, speed, mode):
-        if mode == 'pwm':
+        if mode == "pwm":
             self.set_channel_pwm(channel)
         else:
             self.set_channel_speed(channel, speed)
