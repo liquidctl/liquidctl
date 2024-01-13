liquidctl/liquidctl

commit 38a33cb4ab221c8020d688f8a82298c3eb56a9c0

tree: remove "experimental" notes and suffixes
In the early days of the project it was important to communicate to our
users how well they could expect a device to work with liquidctl. This
was done, for new or still unstable devices, with an `e`/experimental
note (in the supported devices table) and an `(experimental)` suffix in
their description. However, it doesn't make sense to do that anymore:

- we have many more drivers, supported devices, and contributors, and so
  our capability to *accurately* gauge that, across all configurations,
  use cases, firmware versions, etc, has likely degraded;
- our development pace has reduced substantially, degrading the temporal
  resolution (and, thus, accuracy) of any such claim;
- and, on the other hand, *most drivers* either work well from the start
  or have quickly and easily been fixed.

Exceptionally, `(experimental)` suffixes have been kept for a few
untested EVGA and ASUS GPUs, since their support require the passing of
corresponding unsafe flags to be enabled.

liquidctl/cli.py
@@ -164,8 +164,7 @@ def _list_devices_objs(devices):
 
     return [
         {
-            # replace the experimental suffix with a proper field
-            'description': dev.description.replace(' (experimental)', ''),
+            'description': dev.description,
             'vendor_id': dev.vendor_id,
             'product_id': dev.product_id,
             'release_number': dev.release_number,
@@ -174,7 +173,7 @@ def _list_devices_objs(devices):
             'address': dev.address,
             'port': dev.port,
             'driver': type(dev).__name__,
-            'experimental': dev.description.endswith('(experimental)'),
+            'experimental': False, # removed, kept only for backward compatibility
         }
         for dev in devices
     ]
@@ -245,11 +244,10 @@ def _dev_status_obj(dev, status):
             unit = 's'
         return { 'key': key, 'value': val, 'unit': unit }
 
-    # suppress the experimental suffix, `list` reports it in `.experimental`
     return {
         'bus': dev.bus,
         'address': dev.address,
-        'description': dev.description.replace(' (experimental)', ''),
+        'description': dev.description,
         'status': [convert(x) for x in status]
     }
 

extra/linux/generate-uaccess-udev-rules.py
@@ -103,7 +103,6 @@ if __name__ == "__main__":
     for svid, did, sdid, descriptions in nvidia_devs:
         print()
         for desc in descriptions:
-            desc = desc.replace(" (experimental)", "")
             print(f"# {desc}")
         entry = f"""
             KERNEL=="i2c-*", ATTR{{name}}=="NVIDIA i2c adapter 1 *", ATTRS{{vendor}}=="0x10de", \\
@@ -134,7 +133,6 @@ if __name__ == "__main__":
     for vid, pid, descriptions in usb_devs:
         print()
         for desc in descriptions:
-            desc = desc.replace(" (experimental)", "")
             print(f"# {desc}")
         print(
             f'SUBSYSTEMS=="usb", ATTRS{{idVendor}}=="{vid:04x}", ATTRS{{idProduct}}=="{pid:04x}", TAG+="uaccess"'

extra/prometheus-liquidctl-exporter
@@ -78,7 +78,7 @@ class LiquidCollector(object):
 
                     if unit != '':
                         # FIXME doesn't handle multiple equal devices well
-                        label_values = [d.description.replace(' (experimental)', ''), sanitized_name, unit]
+                        label_values = [d.description, sanitized_name, unit]
                         g.add_metric(label_values, value=sample_value)
                         LOGGER.debug(
                             '%s: %s as GaugeMetric %s labels %s',

liquidctl/driver/aquacomputer.py
@@ -166,25 +166,25 @@ class Aquacomputer(UsbHidDriver):
         (
             0x0C70,
             0xF00E,
-            "Aquacomputer D5 Next (experimental)",
+            "Aquacomputer D5 Next",
             {"device_info": _DEVICE_INFO[_DEVICE_D5NEXT]},
         ),
         (
             0x0C70,
             0xF010,
-            "Aquacomputer Farbwerk 360 (experimental)",
+            "Aquacomputer Farbwerk 360",
             {"device_info": _DEVICE_INFO[_DEVICE_FARBWERK360]},
         ),
         (
             0x0C70,
             0xF011,
-            "Aquacomputer Octo (experimental)",
+            "Aquacomputer Octo",
             {"device_info": _DEVICE_INFO[_DEVICE_OCTO]},
         ),
         (
             0x0C70,
             0xF00D,
-            "Aquacomputer Quadro (experimental)",
+            "Aquacomputer Quadro",
             {"device_info": _DEVICE_INFO[_DEVICE_QUADRO]},
         ),
     ]

liquidctl/driver/asetek.py
@@ -436,9 +436,9 @@ class Hydro690Lc(_ModernBase690Lc):
     """Corsair-branded fifth generation Asetek 690LC cooler."""
 
     _MATCHES = [
-        (0x1b1c, 0x0c02, 'Corsair Hydro H80i GT (experimental)', {}),
-        (0x1b1c, 0x0c03, 'Corsair Hydro H100i GTX (experimental)', {}),
-        (0x1b1c, 0x0c07, 'Corsair Hydro H110i GTX (experimental)', {}),
+        (0x1b1c, 0x0c02, 'Corsair Hydro H80i GT', {}),
+        (0x1b1c, 0x0c03, 'Corsair Hydro H100i GTX', {}),
+        (0x1b1c, 0x0c07, 'Corsair Hydro H110i GTX', {}),
         (0x1b1c, 0x0c08, 'Corsair Hydro H80i v2', {}),
         (0x1b1c, 0x0c09, 'Corsair Hydro H100i v2', {}),
         (0x1b1c, 0x0c0a, 'Corsair Hydro H115i', {}),

liquidctl/driver/asus_ryujin.py
@@ -41,7 +41,7 @@ class AsusRyujin(UsbHidDriver):
     """ASUS Ryujin II liquid cooler."""
 
     _MATCHES = [
-        (0x0B05, 0x1988, "ASUS Ryujin II 360 (experimental)", {}),
+        (0x0B05, 0x1988, "ASUS Ryujin II 360", {}),
     ]
 
     def initialize(self, **kwargs):

liquidctl/driver/commander_core.py
@@ -51,9 +51,9 @@ class CommanderCore(UsbHidDriver):
     """Corsair Commander Core"""
 
     _MATCHES = [
-        (0x1b1c, 0x0c1c, 'Corsair Commander Core (experimental)', {"has_pump": True}),
-        (0x1b1c, 0x0c2a, 'Corsair Commander Core XT (experimental)', {"has_pump": False}),
-        (0x1b1c, 0x0c32, 'Corsair Commander ST (experimental)', {"has_pump": True}),
+        (0x1b1c, 0x0c1c, 'Corsair Commander Core', {"has_pump": True}),
+        (0x1b1c, 0x0c2a, 'Corsair Commander Core XT', {"has_pump": False}),
+        (0x1b1c, 0x0c32, 'Corsair Commander ST', {"has_pump": True}),
     ]
 
     def __init__(self, device, description, has_pump, **kwargs):

liquidctl/driver/corsair_hid_psu.py
@@ -100,12 +100,12 @@ class CorsairHidPsu(UsbHidDriver):
             'fpowin115': (0.00010018433053123574, 1.0272313660072225, 14.092187353321624),
             'fpowin230': ( 8.600634771656125e-05, 1.0289245073649413, 13.701515390258626),
         }),
-        (0x1b1c, 0x1c1e, 'Corsair HX1000i (2022) (experimental)', {  # CP-9020214-NA
+        (0x1b1c, 0x1c1e, 'Corsair HX1000i (2022)', {  # CP-9020214-NA
             # FIXME does not accurately represent known efficiency curve at 2%–10% loads
             'fpowin115': (0.12038623471806167, 0.9899868098825505, 0.01312560158286225),
             'fpowin230': (0.08725695213088608, 1.0017598020558338, 0.009789546119565251),
         }),
-        (0x1b1c, 0x1c1f, 'Corsair HX1500i (experimental)', {  # CP-9020215-NA
+        (0x1b1c, 0x1c1f, 'Corsair HX1500i', {  # CP-9020215-NA
             # FIXME does not accurately represent known efficiency curve at 2%–10% loads
             'fpowin115': (0.099089523528618,   1.0125991459969408, 0.011978189078640221),
             'fpowin230': (0.06951642350659733, 1.0183515407262729, 0.011039762908188666),

commit c1cb21e2e2c8d07a7717e9b520cdeeb2f3c2b5b2

cli: prevent contradiction in --json description vs experimental fields
Fixes: 38a33cb4ab22 ("tree: remove "experimental" notes and suffixes")

liquidctl/cli.py
@@ -173,7 +173,8 @@ def _list_devices_objs(devices):
             'address': dev.address,
             'port': dev.port,
             'driver': type(dev).__name__,
-            'experimental': False, # removed, kept only for backward compatibility
+            # deprecated:
+            'experimental': '(experimental)' in dev.description,
         }
         for dev in devices
     ]