liquidctl/liquidctl

commit 2e4e424939bc67b09c475fa05d739445e5af39cf

contest: ignore prometheus-liquidctl-exporter due to additional dependency

conftest.py
@@ -1,10 +1,13 @@
 import sys
 
-collect_ignore = ['setup.py']
-collect_ignore.append('extra/contrib/fusion_rgb_cycle.py')  # depends on coloraide
+collect_ignore = [
+    "setup.py",
+    "extra/contrib/fusion_rgb_cycle.py",  # depends on coloraide
+    "extra/prometheus-liquidctl-exporter",  # depends on prometheus_client
+]
 
-if sys.platform != 'linux':
-    collect_ignore.append('tests/test_smbus.py')
+if sys.platform != "linux":
+    collect_ignore.append("tests/test_smbus.py")
 
-if sys.platform not in ['win32', 'cygwin']:
-    collect_ignore.append('extra/windows/LQiNFO.py')
+if sys.platform not in ["win32", "cygwin"]:
+    collect_ignore.append("extra/windows/LQiNFO.py")

pyproject.toml
@@ -28,7 +28,6 @@ extend-exclude = '''
 
   # exclude some old files, pending a future conversion; by default, any new
   # files should *not* be excluded
-  | (^/conftest\.py$)
   | (^/extra/contrib/fusion_rgb_cycle\.py$)
   | (^/extra/windows/LQiNFO\.py$)
   | (^/liquidctl/cli\.py$)

commit d831273145f2d672835b4fd05e74668af288e16f

conftest: ignore prometheus-liquidctl-exporter with correct filename
Fixes: 2e4e424939bc

conftest.py
@@ -3,7 +3,7 @@ import sys
 collect_ignore = [
     "setup.py",
     "extra/contrib/fusion_rgb_cycle.py",  # depends on coloraide
-    "extra/prometheus-liquidctl-exporter",  # depends on prometheus_client
+    "extra/prometheus-liquidctl-exporter.py",  # depends on prometheus_client
 ]
 
 if sys.platform != "linux":