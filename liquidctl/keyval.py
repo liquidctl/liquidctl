liquidctl/liquidctl

commit d8da208c7b3defef4bd6ee7bfdab63d517767dc8

keyval: remove the use of temporary or auxiliary files

liquidctl/keyval.py
@@ -66,21 +66,21 @@ class _FilesystemBackend:
             os.chmod(self._write_dir, 0o1700)
         _LOGGER.debug('data in %s', self._write_dir)
 
-    def load(self, key, locked=False):
+    def load(self, key):
+        _LOGGER.debug('%r', self._read_dirs)
         for base in self._read_dirs:
             path = os.path.join(base, key)
+            _LOGGER.debug('%s', key)
             if not os.path.isfile(path):
                 continue
             try:
+                with self._open_with_lock(path, os.O_RDONLY, shared=True) as f:
+                    data = f.read().strip()
 
-                with self._shared_lock(key, locked):
-                    with open(path, mode='r') as f:
-                        data = f.read().strip()
+                if not data:
+                    continue
 
-                if len(data) == 0:
-                    value = None
-                else:
-                    value = literal_eval(data)
+                value = literal_eval(data)
                 _LOGGER.debug('loaded %s=%r (from %s)', key, value, path)
             except OSError as err:
                 _LOGGER.warning('%s exists but could not be read: %s', path, err)
@@ -92,67 +92,84 @@ class _FilesystemBackend:
         _LOGGER.debug('no data (file) found for %s', key)
         return None
 
-    def store(self, key, value, locked=False):
+    def store(self, key, value):
         data = repr(value)
         assert literal_eval(data) == value, 'encode/decode roundtrip fails'
         path = os.path.join(self._write_dir, key)
 
-        with self._exclusive_lock(key, locked):
-            fd, tmp = tempfile.mkstemp(dir=self._write_dir, text=True)
-            with open(fd, mode='w') as f:
-                f.write(data)
-                f.flush()
-            os.replace(tmp, path)
+        with self._open_with_lock(path, os.O_WRONLY | os.O_CREAT | os.O_TRUNC) as f:
+            f.write(data)
+            f.flush()  # ensure flushing before automatic unlocking
 
         _LOGGER.debug('stored %s=%r (in %s)', key, value, path)
 
-    def _lock_file(self, key):
-        return os.path.join(self._write_dir, f'{key}.lock')
-
     def load_store(self, key, func):
-        with self._exclusive_lock(key):
-            value = self.load(key, locked=True)
-            newValue = func(value)
-            self.store(key, newValue, locked=True)
+        value = None
+        new_value = None
 
+        path = os.path.join(self._write_dir, key)
 
-    @contextmanager
-    def _shared_lock(self, key, locked=False):
+        # lock the destination as soon as possible
+        with self._open_with_lock(path, os.O_RDWR | os.O_CREAT) as f:
 
-        lockFile = self._lock_file(key)
-        if not locked:
-            f = os.open(lockFile, os.O_CREAT | os.O_RDONLY)
-            if sys.platform == 'win32':
-                msvcrt.locking(f, msvcrt.LK_LOCK, 1)
-            else:
-                fcntl.flock(f, fcntl.LOCK_SH)
+            # still traverse all possible locations to find the current value
+            for base in self._read_dirs:
+                read_path = os.path.join(base, key)
+                if not os.path.isfile(read_path):
+                    continue
+                try:
+                    if read_path == path:
+                        # we already have an exclusive lock to this file
+                        data = f.read().strip()
+                        f.seek(0)
+                    else:
+                        # it appears that in some cases, if `read_file` and `path` point
+                        # to the same file, acquiring a shared lock on `aux` could
+                        # downgrade our lock on `f`
+                        with self._open_with_lock(read_path, os.O_RDWR) as aux:
+                            data = aux.read().strip()
 
-        try:
-            yield
-        finally:
-            if not locked:
-                if sys.platform == 'win32':
-                    msvcrt.locking(f, msvcrt.LK_UNLCK, 1)
+                    if not data:
+                        continue
+
+                    value = literal_eval(data)
+                    _LOGGER.debug('loaded %s=%r (from %s)', key, value, read_path)
+                except OSError as err:
+                    _LOGGER.warning('%s exists but could not be read: %s', read_path, err)
+                except ValueError as err:
+                    _LOGGER.warning('%s exists but was corrupted: %s', key, err)
                 else:
-                    fcntl.flock(f, fcntl.LOCK_UN)
+                    break
+            else:
+                _LOGGER.debug('no data (file) found for %s', key)
+
+            new_value = func(value)
+
+            data = repr(new_value)
+            assert literal_eval(data) == new_value, 'encode/decode roundtrip fails'
+            f.write(data)
+            f.flush()  # ensure flushing before automatic unlocking
+
+            _LOGGER.debug('replaced with %s=%r (stored in %s)', key, new_value, path)
+
+        return (value, new_value)
 
     @contextmanager
-    def _exclusive_lock(self, key, locked=False):
-
-        lockFile = self._lock_file(key)
-        if not locked:
-            f = os.open(lockFile, os.O_CREAT | os.O_RDONLY)
+    def _open_with_lock(self, path, flags, *, shared=False):
+        with os.fdopen(os.open(path, flags), mode='r+') as f:
             if sys.platform == 'win32':
-                msvcrt.locking(f, msvcrt.LK_LOCK, 1)
+                msvcrt.locking(f.fileno(), msvcrt.LK_LOCK, 1)
+            elif shared:
+                fcntl.flock(f, fcntl.LOCK_SH)
             else:
                 fcntl.flock(f, fcntl.LOCK_EX)
 
-        try:
-            yield
-        finally:
-            if not locked:
+            try:
+                yield f
+            finally:
                 if sys.platform == 'win32':
-                    msvcrt.locking(f, msvcrt.LK_UNLCK, 1)
+                    f.seek(0)
+                    msvcrt.locking(f.fileno(), msvcrt.LK_UNLCK, 1)
                 else:
                     fcntl.flock(f, fcntl.LOCK_UN)
 
@@ -179,7 +196,6 @@ class RuntimeStorage:
         """Unstable API."""
 
         def l(value):
-
             if value is None:
                 value = default
             elif of_type and not isinstance(value, of_type):

tests/test_keyval.py
@@ -8,8 +8,22 @@ from pathlib import Path
 from liquidctl.keyval import _FilesystemBackend
 
 
-def test_fs_backend_handles_values_corupted_with_nulls(tmpdir, caplog):
+def test_fs_backend_loads_from_fallback_dir(tmpdir):
+    run_dir = tmpdir.mkdir('run_dir')
+    fb_dir = tmpdir.mkdir('fb_dir')
 
+    fallback = _FilesystemBackend(key_prefixes=['prefix'], runtime_dirs=[fb_dir])
+    fallback.store('key', 42)
+
+    store = _FilesystemBackend(key_prefixes=['prefix'], runtime_dirs=[run_dir, fb_dir])
+    assert store.load('key') == 42
+
+    store.store('key', -1)
+    assert store.load('key') == -1
+    assert fallback.load('key') == 42, 'fallback location was changed'
+
+
+def test_fs_backend_handles_values_corupted_with_nulls(tmpdir, caplog):
     run_dir = tmpdir.mkdir('run_dir')
     store = _FilesystemBackend(key_prefixes=['prefix'], runtime_dirs=[run_dir])
 
@@ -24,198 +38,101 @@ def test_fs_backend_handles_values_corupted_with_nulls(tmpdir, caplog):
     assert 'was corrupted' in caplog.text
 
 
-def test_fs_backend_load_store(tmpdir):
+def test_fs_backend_load_store_returns_old_and_new_values(tmpdir):
     run_dir = tmpdir.mkdir('run_dir')
 
     store = _FilesystemBackend(key_prefixes=['prefix'], runtime_dirs=[run_dir])
     store.store('key', 42)
 
-    p1 = Process(target=_mp_increment_key, args=(run_dir, 'prefix', 'key', .5))
-    p2 = Process(target=_mp_increment_key, args=(run_dir, 'prefix', 'key', .5))
-    p3 = Process(target=_mp_increment_key, args=(run_dir, 'prefix', 'key', .5))
-
-    start_time = time.monotonic()
-    p1.start()
-    p2.start()
-    p3.start()
-
-    p1.join()
-    p2.join()
-    p3.join()
-
-    end_time = time.monotonic()
-    elapsed = (end_time-start_time)
-
-    val = store.load('key')
-
-    assert val == 45
-    assert elapsed >= 1.5
+    assert store.load_store('key', lambda x: x + 1) == (42, 43)
 
 
-@pytest.mark.parametrize('key', [
-    'key1', 'key-value', 'new_key', 'ob1', 'abracadabra'
-    ])
-def test_fs_backend_lock_file(tmpdir, key):
-
+def test_fs_backend_load_store_loads_from_fallback_dir(tmpdir):
     run_dir = tmpdir.mkdir('run_dir')
-    store = _FilesystemBackend(key_prefixes=['prefix'], runtime_dirs=[run_dir])
+    fb_dir = tmpdir.mkdir('fb_dir')
 
-    file = store._lock_file(key)
-    assert os.path.basename(file) == f'{key}.lock'
+    fallback = _FilesystemBackend(key_prefixes=['prefix'], runtime_dirs=[fb_dir])
+    fallback.store('key', 42)
 
+    store = _FilesystemBackend(key_prefixes=['prefix'], runtime_dirs=[run_dir, fb_dir])
+    assert store.load_store('key', lambda x: x + 1) == (42, 43)
 
-def test_fs_backend_shared_lock_locked(tmpdir):
-
-    run_dir = tmpdir.mkdir('run_dir')
-    store = _FilesystemBackend(key_prefixes=['prefix'], runtime_dirs=[run_dir])
-
-    with store._shared_lock('key', locked=True):
-        assert not os.path.exists(store._lock_file('key'))
+    assert fallback.load('key') == 42, 'fallback location was changed'
 
 
-def test_fs_backend_exclusive_lock_locked(tmpdir):
-
-    run_dir = tmpdir.mkdir('run_dir')
-    store = _FilesystemBackend(key_prefixes=['prefix'], runtime_dirs=[run_dir])
-
-    with store._exclusive_lock('key', locked=True):
-        assert not os.path.exists(store._lock_file('key'))
-
-
-def test_fs_backend_shared_lock(tmpdir):
-
-    run_dir = tmpdir.mkdir('run_dir')
-    store = _FilesystemBackend(key_prefixes=['prefix'], runtime_dirs=[run_dir])
-
-    with store._shared_lock('key'):
-        assert os.path.exists(store._lock_file('key'))
-
-    assert os.path.exists(store._lock_file('key'))
-
-
-def test_fs_backend_exclusive_lock(tmpdir):
-
-    run_dir = tmpdir.mkdir('run_dir')
-    store = _FilesystemBackend(key_prefixes=['prefix'], runtime_dirs=[run_dir])
-
-    with store._exclusive_lock('key'):
-        assert os.path.exists(store._lock_file('key'))
-
-    assert os.path.exists(store._lock_file('key'))
-
-
-def test_fs_backend_share_lock(tmpdir):
+def test_fs_backend_load_store_is_atomic(tmpdir):
     run_dir = tmpdir.mkdir('run_dir')
 
     store = _FilesystemBackend(key_prefixes=['prefix'], runtime_dirs=[run_dir])
     store.store('key', 42)
 
-    p1 = Process(target=_mp_shared_sleep, args=(run_dir, 'prefix', 'key', .5))
-    p2 = Process(target=_mp_shared_sleep, args=(run_dir, 'prefix', 'key', .5))
-    p3 = Process(target=_mp_shared_sleep, args=(run_dir, 'prefix', 'key', .5))
+    ps = [
+        Process(target=_fs_mp_increment_key, args=(run_dir, 'prefix', 'key', .5)),
+        Process(target=_fs_mp_increment_key, args=(run_dir, 'prefix', 'key', .5)),
+        Process(target=_fs_mp_increment_key, args=(run_dir, 'prefix', 'key', .5)),
+    ]
 
     start_time = time.monotonic()
-    p1.start()
-    p2.start()
-    p3.start()
 
-    p1.join()
-    p2.join()
-    p3.join()
+    for p in ps:
+        p.start()
 
-    end_time = time.monotonic()
-    elapsed = (end_time-start_time)
+    for p in ps:
+        p.join()
 
-    if sys.platform == 'win32':
-        # no shared locks on windows
-        assert elapsed >= 1.5
-    else:
-        assert 0.5 <= elapsed < 1.5
+    elapsed = (time.monotonic() - start_time)
+
+    assert store.load('key') == 45
+    assert elapsed >= .5 * len(ps)
 
 
-def test_fs_backend_exclusive_lock(tmpdir):
+def test_fs_backend_loads_honor_load_store_locking(tmpdir):
     run_dir = tmpdir.mkdir('run_dir')
 
     store = _FilesystemBackend(key_prefixes=['prefix'], runtime_dirs=[run_dir])
     store.store('key', 42)
 
-    p1 = Process(target=_mp_exclusive_sleep, args=(run_dir, 'prefix', 'key', .5))
-    p2 = Process(target=_mp_exclusive_sleep, args=(run_dir, 'prefix', 'key', .5))
+    ps = [
+        Process(target=_fs_mp_increment_key, args=(run_dir, 'prefix', 'key', .5)),
+        Process(target=_fs_mp_check_key, args=(run_dir, 'prefix', 'key', 43)),
+    ]
 
-    start_time = time.monotonic()
-    p1.start()
-    p2.start()
+    ps[0].start()
+    time.sleep(.1)
+    ps[1].start()
 
-    p1.join()
-    p2.join()
-
-    end_time = time.monotonic()
-    elapsed = (end_time-start_time)
-
-    assert elapsed >= 1
+    for p in ps:
+        p.join()
 
 
-def test_fs_backend_mixed_lock_exclusive_first(tmpdir):
+def test_fs_backend_stores_honor_load_store_locking(tmpdir):
     run_dir = tmpdir.mkdir('run_dir')
 
     store = _FilesystemBackend(key_prefixes=['prefix'], runtime_dirs=[run_dir])
     store.store('key', 42)
 
-    p1 = Process(target=_mp_exclusive_sleep, args=(run_dir, 'prefix', 'key', .5))
-    p2 = Process(target=_mp_shared_sleep, args=(run_dir, 'prefix', 'key', .5))
-    p3 = Process(target=_mp_shared_sleep, args=(run_dir, 'prefix', 'key', .5))
+    ps = [
+        Process(target=_fs_mp_increment_key, args=(run_dir, 'prefix', 'key', .5)),
+        Process(target=_fs_mp_store_key, args=(run_dir, 'prefix', 'key', -1)),
+    ]
 
     start_time = time.monotonic()
-    p1.start()
-    time.sleep(0.1)
-    p2.start()
-    p3.start()
 
-    p1.join()
-    p2.join()
-    p3.join()
+    ps[0].start()
+    time.sleep(.1)
+    ps[1].start()
 
-    end_time = time.monotonic()
-    elapsed = (end_time-start_time)
+    # join second process first
+    ps[1].join()
 
-    if sys.platform == 'win32':
-        # no shared locks on windows
-        assert elapsed >= 1.5
-    else:
-        assert 1 <= elapsed < 1.5
+    elapsed = (time.monotonic() - start_time)
+    assert elapsed >= .5
+
+    ps[0].join()
+    assert store.load('key') == -1
 
 
-def test_fs_backend_mixed_lock_shared_first(tmpdir):
-    run_dir = tmpdir.mkdir('run_dir')
-
-    store = _FilesystemBackend(key_prefixes=['prefix'], runtime_dirs=[run_dir])
-    store.store('key', 42)
-
-    p1 = Process(target=_mp_shared_sleep, args=(run_dir, 'prefix', 'key', .5))
-    p2 = Process(target=_mp_shared_sleep, args=(run_dir, 'prefix', 'key', .5))
-    p3 = Process(target=_mp_exclusive_sleep, args=(run_dir, 'prefix', 'key', .5))
-
-    start_time = time.monotonic()
-    p1.start()
-    p2.start()
-    time.sleep(0.1)
-    p3.start()
-
-    p1.join()
-    p2.join()
-    p3.join()
-
-    end_time = time.monotonic()
-    elapsed = (end_time-start_time)
-
-    if sys.platform == 'win32':
-        # no shared locks on windows
-        assert elapsed >= 1.5
-    else:
-        assert 1 <= elapsed < 1.5
-
-
-def _mp_increment_key(run_dir, prefix, key, sleep):
+def _fs_mp_increment_key(run_dir, prefix, key, sleep):
     """Open a _FilesystemBackend and increment `key`.
 
     For the `multiprocessing` tests.
@@ -232,31 +149,25 @@ def _mp_increment_key(run_dir, prefix, key, sleep):
     store.load_store(key, l)
 
 
-def _mp_exclusive_sleep(run_dir, prefix, key, sleep):
-    """Open a _FilesystemBackend and sleep on `key` holding a exclusive lock.
+def _fs_mp_check_key(run_dir, prefix, key, expected):
+    """Open a _FilesystemBackend and check `key` value against `expected`.
 
     For the `multiprocessing` tests.
 
-    Opens the storage on `run_dir` and with `prefix`.  Sleeps for `sleep`
-    seconds.
+    Opens the storage on `run_dir` and with `prefix`.
     """
 
-    store = _FilesystemBackend(key_prefixes=[prefix], runtime_dirs=[run_dir])
-
-    with store._exclusive_lock(key):
-        time.sleep(sleep)
+    store = _FilesystemBackend(key_prefixes=['prefix'], runtime_dirs=[run_dir])
+    assert store.load(key) == expected
 
 
-def _mp_shared_sleep(run_dir, prefix, key, sleep):
-    """Open a _FilesystemBackend and sleep on `key` holding a shared lock.
+def _fs_mp_store_key(run_dir, prefix, key, new_value):
+    """Open a _FilesystemBackend and store `new_value` for `key`.
 
     For the `multiprocessing` tests.
 
-    Opens the storage on `run_dir` and with `prefix`.  Sleeps for `sleep`
-    seconds.
+    Opens the storage on `run_dir` and with `prefix`.
     """
 
-    store = _FilesystemBackend(key_prefixes=[prefix], runtime_dirs=[run_dir])
-
-    with store._shared_lock(key):
-        time.sleep(sleep)
+    store = _FilesystemBackend(key_prefixes=['prefix'], runtime_dirs=[run_dir])
+    store.store(key, new_value)

commit 3637c50e14388a7af4093161d195be3dd494d433

keyval: do not set +x on newly created files
Pass 0o666 into os.open() because we do not intend the newly created
files to be executable. NB: we do not pass 0o644 or 0o600 here because
the unwanted access mode bits will be further pruned by the umask
which can in turn be overridden by applicable default ACLs.

Fixes: d8da208c7b3defef4bd6ee7bfdab63d517767dc8

liquidctl/keyval.py
@@ -61,7 +61,7 @@ def _open_with_lock(path, flags, *, shared=False):
     else:
         raise ValueError(f'Invalid os.open() flags: {flags}')
 
-    with os.fdopen(os.open(path, flags), mode=write_mode) as f:
+    with os.fdopen(os.open(path, flags, 0o666), mode=write_mode) as f:
         if sys.platform == 'win32':
             msvcrt.locking(f.fileno(), msvcrt.LK_LOCK, 1)
         elif shared: