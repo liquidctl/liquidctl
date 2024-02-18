liquidctl/liquidctl

commit 4706ceb6ef055991455bb854f8fdd56b3339e3c2

Switch to a managed key-value storage and conform to XDG basedir spec
On Linux and other platforms the new RuntimeStorage object conforms to
the XDG basedir spec and supports XDG_RUNTIME_DIR, falling back to
/var/run, and reads check both possible locations.  On the other hand,
/usr[/local]/share is no longer a candidate location.

The subdirectories used by the Legacy 690LC driver have dropped the
release number, but now include 'legacy' as a reference to their owner.

The base directory on Windows no longer includes the appauthor, but
remains whatever is computed by appdirs.site_data_dir.  Appdirs appears
to solve quite a number of potential issues and we don't to deal with
them here (yet?).

There have been no effective changes to the base path on MacOS.

liquidctl/keyval.py
@@ -1,0 +1,112 @@
+"""Simple key-value based storage for liquidctl drivers.
+
+Copyright (C) 2019–2019  Jonas Malaco
+Copyright (C) 2019–2019  each contribution's author
+
+This file is part of liquidctl.
+
+liquidctl is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+liquidctl is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import logging
+import os
+import sys
+
+import appdirs
+
+LOGGER = logging.getLogger(__name__)
+XDG_RUNTIME_DIR = os.getenv('XDG_RUNTIME_DIR')
+
+
+def get_runtime_dirs(appname='liquidctl'):
+    """Return base directories for application runtime data.
+
+    Directories are returned in order of preference.
+    """
+    if sys.platform == 'win32':
+        dirs = [appdirs.site_data_dir(appname, appauthor=False)]
+    elif sys.platform == 'darwin':
+        dirs = [os.path.join('/Library/Application Support', appname)]
+    else:
+        # threat all other platforms as *nix and conform to XDG basedir spec
+        dirs = []
+        if XDG_RUNTIME_DIR:
+            dirs.append(os.path.join(XDG_RUNTIME_DIR, appname))
+        # regardless whether XDG_RUNTIME_DIR is set, fallback to /var/run if it
+        # is available; this allows a user with XDG_RUNTIME_DIR set to still
+        # find data stored by another user as long as it is in the fallback
+        # path (see #37 for a real world use case)
+        if os.path.isdir('/var/run'):
+            dirs.append(os.path.join('/var/run', appname))
+        assert dirs, 'Could not get a suitable place to store runtime data'
+    return dirs
+
+
+class RuntimeStorage:
+    """Unstable API."""
+
+    def __init__(self, key_prefixes):
+        for prefix in key_prefixes:
+            assert not '..' in prefix
+            assert not os.path.isabs(prefix)
+        self._cache = {}
+        # compute read and write dirs from base runtime dirs: the first base
+        # dir is selected for writes and prefered for reads
+        self._read_dirs = [os.path.join(x, *key_prefixes) for x in get_runtime_dirs()]
+        self._write_dir = self._read_dirs[0]
+        # prepare the write dir
+        os.makedirs(self._write_dir, exist_ok=True)
+        if XDG_RUNTIME_DIR and os.path.commonpath([XDG_RUNTIME_DIR, self._write_dir]):
+            # set the sticky bit to prevent removal during cleanup
+            os.chmod(self._write_dir, 0o1700)
+            LOGGER.debug('data in %s (within XDG_RUNTIME_DIR)', self._write_dir)
+        else:
+            LOGGER.debug('data in %s', self._write_dir)
+
+    def load_int(self, key, default=None):
+        """Unstable API."""
+        if key in self._cache:
+            value = self._cache[key]
+            LOGGER.debug('loaded %s=%s (from cache)', key, str(value))
+            return value if value is not None else default
+        for base in self._read_dirs:
+            path = os.path.join(base, key)
+            if not os.path.isfile(path):
+                continue
+            try:
+                with open(path, mode='r') as f:
+                    data = f.read().strip()
+                    if len(data) == 0:
+                        value = None
+                    else:
+                        value = int(data)
+                    LOGGER.debug('loaded %s=%s (from %s)', key, str(value), path)
+            except OSError as err:
+                LOGGER.warning('%s exists but cannot be read: %s', path, err)
+                continue
+            self._cache[key] = value
+            return value if value is not None else default
+        LOGGER.debug('no data (file) found for %s', key)
+        return default
+
+    def store_int(self, key, value):
+        """Unstable API."""
+        path = os.path.join(self._write_dir, key)
+        with open(path, mode='w') as f:
+            if value is None:
+                f.write('')
+            else:
+                f.write(str(value))
+            self._cache[key] = value
+            LOGGER.debug('stored %s=%s (in %s)', key, str(value), path)

liquidctl/driver/asetek.py
@@ -45,13 +45,11 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import logging
-import os
-import sys
 
-import appdirs
 import usb
 
 from liquidctl.driver.usb import UsbDriver
+from liquidctl.keyval import RuntimeStorage
 from liquidctl.util import clamp
 
 LOGGER = logging.getLogger(__name__)
@@ -302,67 +300,29 @@ class LegacyAsetekDriver(CommonAsetekDriver):
 
     def __init__(self, device, description, **kwargs):
         super().__init__(device, description, **kwargs)
-        # compute path where fan/pump settings will be stored
-        # [/run]/liquidctl/2433_b200_0100/usb1_12/
-        ids = '{:04x}_{:04x}_{:04x}'.format(self.vendor_id, self.product_id, self.release_number)
-        location = '{}_{}'.format(self.bus, '.'.join(map(str, self.port)))
-        if sys.platform.startswith('linux') and os.path.isdir('/run'):
-            basedir = '/run/liquidctl'
-        else:
-            basedir = appdirs.site_data_dir('liquidctl', 'jonasmalacofilho')
-        self._data_path = os.path.join(basedir, ids, location)
-        self._data_cache = {}
+        # --device causes drivers to be instantiated even if they are later
+        # discarded; defer instantiating the data storage until to connect()
+        self._data = None
 
     def connect(self, **kwargs):
-        LOGGER.debug('data directory for device is %s', self._data_path)
         super().connect(**kwargs)
-
-    def _load_integer(self, name, default=None):
-        if name in self._data_cache:
-            value = self._data_cache[name]
-            LOGGER.debug('loaded %s=%s (from cache)', name, str(value))
-            return value if value is not None else default
-        try:
-            with open(os.path.join(self._data_path, name), mode='r') as f:
-                data = f.read().strip()
-                if len(data) == 0:
-                    value = None
-                else:
-                    value = int(data)
-                LOGGER.debug('loaded %s=%s', name, str(value))
-        except OSError:
-            LOGGER.debug('no data (file) found for %s', name)
-            value = None
-        finally:
-            self._data_cache[name] = value
-            return value if value is not None else default
-
-    def _store_integer(self, name, value):
-        try:
-            os.makedirs(self._data_path, mode=0o755)
-        except FileExistsError:
-            pass
-        with open(os.path.join(self._data_path, name), mode='w') as f:
-            if value is None:
-                f.write('')
-            else:
-                f.write(str(value))
-            self._data_cache[name] = value
-            LOGGER.debug('stored %s=%s', name, str(value))
+        ids = '{:04x}_{:04x}'.format(self.vendor_id, self.product_id)
+        loc = '{}_{}'.format(self.bus, '.'.join(map(str, self.port)))
+        self._data = RuntimeStorage(key_prefixes=[ids, loc, 'legacy'])
 
     def _set_all_fixed_speeds(self):
         self._begin_transaction()
         for channel in ['pump', 'fan']:
             mtype, dmin, dmax = _LEGACY_FIXED_SPEED_CHANNELS[channel]
-            duty = clamp(self._load_integer('{}_duty'.format(channel), default=dmax), dmin, dmax)
+            duty = clamp(self._data.load_int('{}_duty'.format(channel), default=dmax), dmin, dmax)
             LOGGER.info('setting %s duty to %i%%', channel, duty)
             self._write([mtype, duty])
         return self._end_transaction_and_read()
 
     def initialize(self, **kwargs):
         super().initialize(**kwargs)
-        self._store_integer('pump_duty', None)
-        self._store_integer('fan_duty', None)
+        self._data.store_int('pump_duty', None)
+        self._data.store_int('fan_duty', None)
         self._set_all_fixed_speeds()
 
     def get_status(self, **kwargs):
@@ -416,7 +376,7 @@ class LegacyAsetekDriver(CommonAsetekDriver):
         """Set channel to a fixed speed duty."""
         mtype, dmin, dmax = _LEGACY_FIXED_SPEED_CHANNELS[channel]
         duty = clamp(duty, dmin, dmax)
-        self._store_integer('{}_duty'.format(channel), duty)
+        self._data.store_int('{}_duty'.format(channel), duty)
         self._set_all_fixed_speeds()
 
 

commit 71395223c3985d046d897fe56d7850668771d041

keyval: do not break ACLs on state directories
Fixes: 4706ceb6ef055991455bb854f8fdd56b3339e3c2

liquidctl/keyval.py
@@ -6,6 +6,7 @@ SPDX-License-Identifier: GPL-3.0-or-later
 
 import logging
 import os
+import stat
 import sys
 import tempfile
 from ast import literal_eval
@@ -69,6 +70,45 @@ def _open_with_lock(path, flags, *, shared=False):
         yield f
 
 
+if {'umask', 'stat', 'chmod'} <= os.__dict__.keys() \
+        and {os.stat, os.chmod} <= os.supports_fd:
+    @contextmanager
+    def _os_open(path, flags, mode=0o777, *, dir_fd=None):
+        """Helper function that wraps os.open() and os.close() in a context manager"""
+        fd = os.open(path, flags, mode, dir_fd=dir_fd)
+        try:
+            yield fd
+        finally:
+            os.close(fd)
+
+    @contextmanager
+    def _umask_bits(mode):
+        """Context manager that temporarily sets the specified bits in the umask of the current process"""
+        old_umask = os.umask(0o777)
+        os.umask(old_umask | mode)
+        try:
+            yield
+        finally:
+            os.umask(old_umask)
+
+    def _chmod_bits(path, mode, *, flags=0):
+        """Helper function that sets the specified bits in the Unix permissions of a file"""
+        with _os_open(path, os.O_RDONLY | flags) as fd:
+            st = os.stat(fd)
+            st_mode = stat.S_IMODE(st.st_mode)
+            # do not chmod() if there's nothing to change -- we might not be the owner
+            if st_mode | mode != st_mode:
+                os.chmod(fd, st_mode | mode)
+
+else:
+    @contextmanager
+    def _umask_bits(umask):
+        yield
+
+    def _chmod_bits(path, mode):
+        pass
+
+
 class _FilesystemBackend:
     def _sanitize(self, key):
         if not isinstance(key, str):
@@ -83,10 +123,13 @@ class _FilesystemBackend:
         # dir is selected for writes and prefered for reads
         self._read_dirs = [os.path.join(x, *key_prefixes) for x in runtime_dirs]
         self._write_dir = self._read_dirs[0]
-        os.makedirs(self._write_dir, exist_ok=True)
-        if sys.platform == 'linux':
+        # create leading directories with default permissions
+        os.makedirs(os.path.dirname(self._write_dir), exist_ok=True)
+        with _umask_bits(0o077):
+            # create leaf directory as 0o0700, but do not break ACLs
+            os.makedirs(self._write_dir, exist_ok=True)
             # set the sticky bit to prevent removal during cleanup
-            os.chmod(self._write_dir, 0o1700)
+            _chmod_bits(self._write_dir, 0o1000, flags=os.O_DIRECTORY)
         _LOGGER.debug('data in %s', self._write_dir)
 
     def load(self, key):