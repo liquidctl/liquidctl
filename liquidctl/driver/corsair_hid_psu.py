liquidctl/liquidctl

commit 3e3f36bd1e4f017d5e36f79a7dcdd8eb8fec46de

corsair_hid_psu: add support for hx1500i and hx1000i 2022 re-issue
PIDs taken from patches to the corsair-psu hwmon driver[1][2].

Efficiency data taken from current manual[3]. For more information, see:

    https://github.com/liquidctl/collected-device-data/tree/master/psu-efficiency

[1] https://lore.kernel.org/linux-hwmon/YznOUQ7Pijedu0NW@monster.localdomain/
[2] https://lore.kernel.org/linux-hwmon/Y0FghqQCHG%2FcX5Jz@monster.localdomain/
[3] https://www.corsair.com/corsairmedia/sys_master/productcontent/49_002614_AA_WW_HXi_Series_QSG_Web.pdf

liquidctl/driver/corsair_hid_psu.py
@@ -76,7 +76,7 @@ class CorsairHidPsu(UsbHidDriver):
             'fpowin115': (0.00011552923724840388, 1.0111311876704099, 12.015296651918918),
             'fpowin230': ( 8.126644224872423e-05, 1.0176256272095185, 10.290640442373850),
         }),
-        (0x1b1c, 0x1c07, 'Corsair HX1000i', {
+        (0x1b1c, 0x1c07, 'Corsair HX1000i', {  # CP-9020074-NA
             'fpowin115': (9.48609754417109e-05,  1.0170509865269720, 11.619826520447452),
             'fpowin230': (9.649987544008507e-05, 1.0018241767296636, 12.759957859756842),
         }),
@@ -99,6 +99,16 @@ class CorsairHidPsu(UsbHidDriver):
         (0x1b1c, 0x1c0d, 'Corsair RM1000i', {
             'fpowin115': (0.00010018433053123574, 1.0272313660072225, 14.092187353321624),
             'fpowin230': ( 8.600634771656125e-05, 1.0289245073649413, 13.701515390258626),
+        }),
+        (0x1b1c, 0x1c1e, 'Corsair HX1000i (2022) (experimental)', {  # CP-9020214-NA
+            # FIXME does not accurately represent known efficiency curve at 2%–10% loads
+            'fpowin115': (0.12038623471806167, 0.9899868098825505, 0.01312560158286225),
+            'fpowin230': (0.08725695213088608, 1.0017598020558338, 0.009789546119565251),
+        }),
+        (0x1b1c, 0x1c1f, 'Corsair HX1500i (experimental)', {  # CP-9020215-NA
+            # FIXME does not accurately represent known efficiency curve at 2%–10% loads
+            'fpowin115': (0.099089523528618,   1.0125991459969408, 0.011978189078640221),
+            'fpowin230': (0.06951642350659733, 1.0183515407262729, 0.011039762908188666),
         }),
     ]
 

extra/linux/71-liquidctl.rules
@@ -362,8 +362,14 @@ SUBSYSTEMS=="usb", ATTRS{idVendor}=="1b1c", ATTRS{idProduct}=="0c10", TAG+="uacc
 # Corsair HX1000i
 SUBSYSTEMS=="usb", ATTRS{idVendor}=="1b1c", ATTRS{idProduct}=="1c07", TAG+="uaccess"
 
+# Corsair HX1000i (2022)
+SUBSYSTEMS=="usb", ATTRS{idVendor}=="1b1c", ATTRS{idProduct}=="1c1e", TAG+="uaccess"
+
 # Corsair HX1200i
 SUBSYSTEMS=="usb", ATTRS{idVendor}=="1b1c", ATTRS{idProduct}=="1c08", TAG+="uaccess"
+
+# Corsair HX1500i
+SUBSYSTEMS=="usb", ATTRS{idVendor}=="1b1c", ATTRS{idProduct}=="1c1f", TAG+="uaccess"
 
 # Corsair HX750i
 SUBSYSTEMS=="usb", ATTRS{idVendor}=="1b1c", ATTRS{idProduct}=="1c05", TAG+="uaccess"

liquidctl.8
@@ -443,7 +443,7 @@ The animation direction can be set with
 .BI \-\-direction= value ,
 where the allowed values are: \fIforward\fR or \fIbackward\fR.
 .
-.SS Corsair HX750i, HX850i, HX1000i, HX1200i
+.SS Corsair HX750i, HX850i, HX1000i, HX1200i, HX1500i
 .SS Corsair RM650i, RM750i, RM850i, RM1000i
 Cooling channels: \fIfan\fR.
 .PP

commit 5f01650fa4ffc871fcbb2b39aefc3e896920fe06

corsair_hid_psu: fix input power curves for HX1000i (2022) and HX1500i
Previously the curves were incorrectly computed in normalized mode,
while liquidctl expects them to yield absolute input power values.

Fixed by regenerating the curves with [`interpolate`] and suitable
`--rated` values.

[`interpolate`]: https://github.com/liquidctl/collected-device-data/blob/master/psu-efficiency/interpolate

Fixes: 3e3f36bd1e4f017d5e36f79a7dcdd8eb8fec46de
Fixes: #675
Related: https://github.com/liquidctl/collected-device-data/commit/4da9df3cfb7dc09831a1d3a6e3d86fb3b16ae92b

liquidctl/driver/corsair_hid_psu.py
@@ -102,13 +102,13 @@ class CorsairHidPsu(UsbHidDriver):
         }),
         (0x1b1c, 0x1c1e, 'Corsair HX1000i (2022)', {  # CP-9020214-NA
             # FIXME does not accurately represent known efficiency curve at 2%–10% loads
-            'fpowin115': (0.12038623471806167, 0.9899868098825505, 0.01312560158286225),
-            'fpowin230': (0.08725695213088608, 1.0017598020558338, 0.009789546119565251),
+            'fpowin115': (0.00012038623467957958, 0.9899868099948035, 13.125601514017152),
+            'fpowin230': ( 8.725695209710315e-05, 1.0017598021499974,  9.789546063300154),
         }),
         (0x1b1c, 0x1c1f, 'Corsair HX1500i', {  # CP-9020215-NA
             # FIXME does not accurately represent known efficiency curve at 2%–10% loads
-            'fpowin115': (0.099089523528618,   1.0125991459969408, 0.011978189078640221),
-            'fpowin230': (0.06951642350659733, 1.0183515407262729, 0.011039762908188666),
+            'fpowin115': (6.605968230747892e-05, 1.0125991461405333, 17.96728350708451),
+            'fpowin230': (4.634428233657273e-05, 1.0183515407387007, 16.559644350684962),
         }),
     ]
 