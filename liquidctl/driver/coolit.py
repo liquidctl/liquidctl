dde803ba2264018a5349c0c4bd7e9dfd1ea84405
Serphentas <Serphentas@users.noreply.github.com>
2024-03-31 16:16:08 +0200

    make linter happy again

--- liquidctl/driver/coolit.py
+++ liquidctl/driver/coolit.py
@@ -156,9 +156,7 @@ class CoolitDriver(UsbHidDriver):
         Returns a list of `(property, value, unit)` tuples.
         """
         if pump_mode not in ["quiet", "extreme"]:
-            LOGGER.warning(
-                'pump mode must be either "quiet" or "extreme", falling back to "quiet"'
-            )
+            LOGGER.warning('pump mode must be either "quiet" or "extreme", falling back to "quiet"')
             pump_mode = "quiet"
 
         self._data.store("pump_mode", _PumpMode[pump_mode.upper()].value)
