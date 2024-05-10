c54972875bbd7a73287514f3dd7cb07dd0144387
Jonas Malaco <jonas@protocubo.io>
2024-05-10 02:04:11 -0300

    smart_device: add comments where channels are disabled due to issues

--- liquidctl/driver/smart_device.py
+++ liquidctl/driver/smart_device.py
@@ -430,11 +430,11 @@ class SmartDevice2(_BaseSmartDevice):
         }),
         (0x1e71, 0x2011, 'NZXT RGB & Fan Controller (3+6 channels)', {
             'speed_channel_count': 3,
-            'color_channel_count': 0
+            'color_channel_count': 0  # protocol changed, see #541
         }),
         (0x1e71, 0x2019, 'NZXT RGB & Fan Controller (3+6 channels)', {
             'speed_channel_count': 3,
-            'color_channel_count': 0
+            'color_channel_count': 0  # protocol changed, see #541
         }),
         (0x1e71, 0x2020, 'NZXT RGB & Fan Controller (3+6 channels)', {
             'speed_channel_count': 3,
