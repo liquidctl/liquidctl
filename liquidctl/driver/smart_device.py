a3ea4161f5659a664ceaa25dc96aeda7d806d88c
Don Pollitz <chaos2007@gmail.com>
2024-09-20 07:37:08 +0200

    smart_device: support new variant of 3+6 RGB & Fan Controller (#733)

--- liquidctl/driver/smart_device.py
+++ liquidctl/driver/smart_device.py
@@ -436,6 +436,10 @@ class SmartDevice2(_BaseSmartDevice):
             'speed_channel_count': 3,
             'color_channel_count': 0  # protocol changed, see #541
         }),
+        (0x1e71, 0x201f, 'NZXT RGB & Fan Controller (3+6 channels)', {
+            'speed_channel_count': 3,
+            'color_channel_count': 0  # protocol changed, see #541
+        }),
         (0x1e71, 0x2020, 'NZXT RGB & Fan Controller (3+6 channels)', {
             'speed_channel_count': 3,
             'color_channel_count': 6
