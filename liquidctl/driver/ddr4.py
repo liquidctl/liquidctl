96f1288c735629ed5349ccd4c20dace31b920b59
Jonas Malaco <jonas@protocubo.io>
2024-09-20 11:25:56 -0300

    ddr4: log the addresses being checked

--- liquidctl/driver/ddr4.py
+++ liquidctl/driver/ddr4.py
@@ -149,6 +149,8 @@ class Ddr4Temperature(SmbusDriver):
 
         for dimm in range(cls._SA_MASK + 1):
             spd_addr = cls._SPD_DTIC | dimm
+            _LOGGER.debug('checking address: %x', spd_addr)
+
             eeprom = smbus.load_eeprom(spd_addr)
 
             if not eeprom or eeprom.name != 'ee1004':
