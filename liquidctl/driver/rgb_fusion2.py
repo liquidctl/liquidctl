liquidctl/liquidctl

commit 4286cc477ddfcfed2b7778a0308e18cfa0ad07b7

rgb_fusion2: linux: don't create driver instance for second usage
Hidapi 0.10 introduces support for multiple usage and usage pages to
hidraw, and may now return multiple handles for a single device
interface.

However, since previous versions of hidapi left the usage_page and usage
field unintialized, rather than set to a knwon invalid value (like 0),
we cannot positively check for the desired usage.

Instead, we check for undesired one, and assume that in all other cases
we either have the desired usage, or that on that system (platform,
hidapi version, and hidapi backend) a single handle is still returned
for the device.

Related: #259 ("Linux with hidapi-0.10.1 is showing RGB Fusion
controller twice")

liquidctl/driver/rgb_fusion2.py
@@ -19,6 +19,9 @@ from liquidctl.util import clamp
 
 LOGGER = logging.getLogger(__name__)
 
+_USAGE_PAGE = 0xff89
+_RGB_CONTROL_USAGE = 0xcc
+_OTHER_USAGE = 0x10
 _REPORT_ID = 0xcc
 _REPORT_BYTE_LENGTH = 63
 _INIT_CMD = 0x60
@@ -104,14 +107,21 @@ class RgbFusion2(UsbHidDriver):
     def probe(cls, handle, **kwargs):
         """Probe `handle` and yield corresponding driver instances.
 
-        These devices have multiple top-level HID usages.  On Windows and Mac
-        each usage results in a different HID handle and, specifically on
-        Windows, only one of them is usable.  So HidapiDevice handles matching
-        other usages have to be ignored.
+        These devices have multiple top-level HID usages, and HidapiDevice
+        handles matching other usages have to be ignored.
         """
 
-        if (not sys.platform.startswith('linux')) and (handle.hidinfo['usage'] != _REPORT_ID):
+        # if usage_page/usage are not available due to hidapi limitations
+        # (version, platform or backend), they are unfortunately left
+        # uninitialized; because of this, we explicitly exclude the undesired
+        # usage_page/usage pair, and assume in all other cases that we either
+        # have the desired usage page/usage pair, or that on that system a
+        # single handle is returned for that device interface (see: 259)
+
+        if (handle.hidinfo['usage_page'] == _USAGE_PAGE and
+            handle.hidinfo['usage'] == _OTHER_USAGE):
             return
+
         yield from super().probe(handle, **kwargs)
 
     def initialize(self, **kwargs):

commit 460bf282aef17e42b7fa146838df5639ddce056c

rgb_fusion2: ignore more possible HID usages with 5711 controller
Fixes: 4286cc477ddf ("rgb_fusion2: linux: don't create driver instance
for second usage")

liquidctl/driver/rgb_fusion2.py
@@ -21,7 +21,6 @@ _LOGGER = logging.getLogger(__name__)
 
 _USAGE_PAGE = 0xff89
 _RGB_CONTROL_USAGE = 0xcc
-_OTHER_USAGE = 0x10
 _REPORT_ID = 0xcc
 _REPORT_BYTE_LENGTH = 63
 _INIT_CMD = 0x60
@@ -113,15 +112,20 @@ class RgbFusion2(UsbHidDriver):
         handles matching other usages have to be ignored.
         """
 
-        # if usage_page/usage are not available due to hidapi limitations
-        # (version, platform or backend), they are unfortunately left
-        # uninitialized; because of this, we explicitly exclude the undesired
-        # usage_page/usage pair, and assume in all other cases that we either
-        # have the desired usage page/usage pair, or that on that system a
-        # single handle is returned for that device interface (see: 259)
+        # A complication is that if HID usage and usage page are not available
+        # to HIDAPI, a single handle is returned for the device interface, but
+        # the usage and usage page fields are unfortunately left uninitialized
+        # by the library (as opposed to in some well defined default value).
+        #
+        # Therefore, assume that a handle with an unknown usage page means that
+        # we're on system where usage and usage page aren't available, and
+        # there's only one handle for the device interface. Otherwise, check
+        # that the usage matches the desired value.
+        #
+        # See: #259 and #759.
 
         if (handle.hidinfo['usage_page'] == _USAGE_PAGE and
-                handle.hidinfo['usage'] == _OTHER_USAGE):
+                handle.hidinfo['usage'] != _RGB_CONTROL_USAGE):
             return
 
         yield from super().probe(handle, **kwargs)